0dc72e4dc5ec1d4f392dafb3b94c682203da76bb
Mainak Jas <mainakjas@gmail.com>
2019-01-30 13:58:21 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+
+descr = """Experimental code for simulating evoked using Neuron"""
+
+DISTNAME = 'mne-neuron'
+DESCRIPTION = descr
+MAINTAINER = 'Mainak Jas'
+MAINTAINER_EMAIL = 'mainakjas@gmail.com'
+URL = ''
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'http://github.com/mne-tools/mne-neuron'
+VERSION = '0.1.dev0'
+
+if __name__ == "__main__":
+    setup(name=DISTNAME,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          long_description=open('README.md').read(),
+          long_description_content_type='text/markdown',
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved',
+              'Programming Language :: Python',
+              'Topic :: Software Development',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS',
+          ],
+          platforms='any',
+          packages=find_packages()
+          )
