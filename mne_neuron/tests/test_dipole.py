b2681b62a05c922f93d6afef5df8017927ec3242
Mainak Jas <mainakjas@gmail.com>
2019-02-21 14:03:28 -0500

    TST dipoles too

---
+++ mne_neuron/tests/test_dipole.py
@@ -0,0 +1,26 @@
+import matplotlib
+import os.path as op
+
+import numpy as np
+
+import mne_neuron
+from mne_neuron import Params
+from mne_neuron.dipole import Dipole
+
+matplotlib.use('agg')
+
+
+def test_dipole():
+    """Test params object."""
+    mne_neuron_root = op.join(op.dirname(mne_neuron.__file__), '..')
+    params_fname = op.join(mne_neuron_root, 'param', 'default.param')
+    params = Params(params_fname)
+
+    times = np.random.random(6000)
+    data = np.random.random((6000, 3))
+    dipole = Dipole(times, data)
+    dipole.baseline_renormalize(params)
+    dipole.convert_fAm_to_nAm()
+    dipole.scale(params['dipole_scalefctr'])
+    dipole.smooth(params['dipole_smooth_win'] / params['dt'])
+    dipole.plot(layer='agg')
