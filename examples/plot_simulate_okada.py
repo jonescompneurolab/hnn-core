c0a95708d37f72e38180ba1b38a49763fc234c6d
Mainak Jas <mainakjas@gmail.com>
2019-08-18 00:59:56 -0400

    DOC WIP: start example to reproduce Okada paper

---
+++ examples/plot_simulate_okada.py
@@ -0,0 +1,21 @@
+"""Reproduce classic Okada constant paper.
+
+References
+----------
+[1] Invariance in current dipole moment density across brain structures and
+species: Physiological constraint for neuroimaging
+
+"""
+
+from mne_neuron.pyramidal import Pyr
+from mne_neuron.basket import Basket
+
+pyr_props = dict(pos=0, L=39.0, diam=28.9,
+                 cm=0.85, Ra=200.0, name='Pyr')
+basket_props = dict(pos=0, L=39.0, diam=20.0,
+                    cm=0.85, Ra=200.0, name='Basket')
+
+pyrs, baskets = list(), list()
+for gid in range(100):
+    pyrs.append(Pyr(gid=gid, soma_props=pyr_props))
+    baskets.append(Basket(gid=gid + 100, pos=0))
