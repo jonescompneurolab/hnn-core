ccca9abd811857f865097afe20c46ff313b62072
Blake Caldwell <blake_caldwell@brown.edu>
2019-09-27 04:57:23 -0400

    TST: add tests for read_params() and Params()

--- hnn_core/tests/test_params.py
+++ hnn_core/tests/test_params.py
@@ -1,15 +1,59 @@
 # Authors: Mainak Jas <mainakjas@gmail.com>
+#          Blake Caldwell <blake_caldwell@brown.edu>
 
 import os.path as op
+import json
+
+import pytest
+from mne.utils import _fetch_file
 
 import hnn_core
-from hnn_core import Params
+from hnn_core import read_params, Params
+hnn_core_root = op.join(op.dirname(hnn_core.__file__), '..')
 
 
 def test_params():
     """Test params object."""
-    hnn_core_root = op.join(op.dirname(hnn_core.__file__), '..')
     params_fname = op.join(hnn_core_root, 'param', 'default.json')
-    params = Params(params_fname)
+    params = read_params(params_fname)
     print(params)
     print(params['L2Pyr*'])
+
+
+def test_legacy_params():
+    """Test reading legacy .param file."""
+    param_url = ('https://raw.githubusercontent.com/hnnsolver/'
+                 'hnn-core/test_data/default.param')
+    params_legacy_fname = op.join(hnn_core_root, 'param', 'default.param')
+    if not op.exists(params_legacy_fname):
+        _fetch_file(param_url, params_legacy_fname)
+
+    params_new_fname = op.join(hnn_core_root, 'param', 'default.json')
+    params_legacy = read_params(params_legacy_fname)
+    params_new = read_params(params_new_fname)
+
+    assert params_new == params_legacy
+
+
+def test_base_params():
+    """Test params object with base params"""
+    param_url = ('https://raw.githubusercontent.com/hnnsolver/'
+                 'hnn-core/test_data/base.json')
+    params_base_fname = op.join(hnn_core_root, 'param', 'base.json')
+    if not op.exists(params_base_fname):
+        _fetch_file(param_url, params_base_fname)
+
+    params_base = read_params(params_base_fname)
+    params = Params()
+    assert params == params_base
+
+    # unsupported extension
+    pytest.raises(ValueError, read_params, 'params.txt')
+    # empty file
+    empty_fname = op.join(hnn_core_root, 'param', 'empty.json')
+    with open(empty_fname, 'w') as json_data:
+        json.dump({}, json_data)
+    pytest.raises(ValueError, read_params, empty_fname)
+    # non dict type
+    pytest.raises(ValueError, Params, [])
+    pytest.raises(ValueError, Params, 'sdfdfdf')
