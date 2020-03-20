3a70720232a70b3d824fb95e9b267db29493cfc0
Blake Caldwell <blake_caldwell@brown.edu>
2020-03-20 16:43:08 -0400

    TST: add joblib and update MPI

--- hnn_core/tests/test_cell.py
+++ hnn_core/tests/test_cell.py
@@ -2,17 +2,17 @@ import matplotlib
 import os.path as op
 
 import hnn_core
-from hnn_core import read_params, Network
+from hnn_core import read_params
+from hnn_core.neuron import _neuron_network
 
 matplotlib.use('agg')
 
 
-def test_dipole():
-    """Test params object."""
+def test_cell():
+    """Test cells object."""
     hnn_core_root = op.join(op.dirname(hnn_core.__file__), '..')
     params_fname = op.join(hnn_core_root, 'param', 'default.json')
     params = read_params(params_fname)
 
-    with Network(params) as net:
-        net.build()
-        net.cells[0].plot_voltage()
+    with _neuron_network(params) as neuron_network:
+        neuron_network.cells[0].plot_voltage()
--- hnn_core/tests/test_compare_hnn.py
+++ hnn_core/tests/test_compare_hnn.py
@@ -1,24 +1,25 @@
 import os.path as op
+import pytest
 
 from numpy import loadtxt
 from numpy.testing import assert_array_equal
 
 from mne.utils import _fetch_file
 import hnn_core
-from hnn_core import simulate_dipole, Network, read_params, get_rank
+from hnn_core import simulate_dipole, Network, read_params
+from hnn_core import MPI_backend, Joblib_backend
 
 
-def test_hnn_core(n_jobs=1):
+def run_hnn_core(backend=None, n_jobs=1):
     """Test to check if hnn-core does not break."""
-    if get_rank() == 0:
-        # small snippet of data on data branch for now. To be deleted
-        # later. Data branch should have only commit so it does not
-        # pollute the history.
-        data_url = ('https://raw.githubusercontent.com/jonescompneurolab/'
-                    'hnn-core/test_data/dpl.txt')
-        if not op.exists('dpl.txt'):
-            _fetch_file(data_url, 'dpl.txt')
-        dpl_master = loadtxt('dpl.txt')
+    # small snippet of data on data branch for now. To be deleted
+    # later. Data branch should have only commit so it does not
+    # pollute the history.
+    data_url = ('https://raw.githubusercontent.com/jonescompneurolab/'
+                'hnn-core/test_data/dpl.txt')
+    if not op.exists('dpl.txt'):
+        _fetch_file(data_url, 'dpl.txt')
+    dpl_master = loadtxt('dpl.txt')
 
     hnn_core_root = op.join(op.dirname(hnn_core.__file__), '..')
 
@@ -28,31 +29,57 @@ def test_hnn_core(n_jobs=1):
 
     # run the simulation
     net = Network(params)
-    dpl = simulate_dipole(net, n_jobs=n_jobs)[0]
 
-    if get_rank() == 0:
-        # write the dipole to a file and compare
-        fname = './dpl2.txt'
-        dpl.write(fname)
+    if backend == 'mpi':
+        with MPI_backend(n_jobs=n_jobs, n_procs=2, mpi_cmd='mpiexec'):
+            dpl = simulate_dipole(net, n_trials=1)[0]
+    elif backend == 'joblib':
+        with Joblib_backend(n_jobs=n_jobs):
+            dpl = simulate_dipole(net, n_trials=2)[0]
+    else:
+        dpl = simulate_dipole(net, n_jobs=n_jobs)[0]
 
-        dpl_pr = loadtxt(fname)
-        assert_array_equal(dpl_pr[:, 2], dpl_master[:, 2])  # L2
-        assert_array_equal(dpl_pr[:, 3], dpl_master[:, 3])  # L5
+    # write the dipole to a file and compare
+    fname = './dpl2.txt'
+    dpl.write(fname)
 
-        # Test spike type counts
-        spiketype_counts = {}
-        for spikegid in net.spikes.gids[0]:
-            if net.gid_to_type(spikegid) not in spiketype_counts:
-                spiketype_counts[net.gid_to_type(spikegid)] = 0
-            else:
-                spiketype_counts[net.gid_to_type(spikegid)] += 1
-        assert 'common' not in spiketype_counts
-        assert 'exgauss' not in spiketype_counts
-        assert 'extpois' not in spiketype_counts
-        assert spiketype_counts == {'evprox1': 269,
-                                    'L2_basket': 54,
-                                    'L2_pyramidal': 113,
-                                    'L5_pyramidal': 395,
-                                    'L5_basket': 85,
-                                    'evdist1': 234,
-                                    'evprox2': 269}
+    dpl_pr = loadtxt(fname)
+    assert_array_equal(dpl_pr[:, 2], dpl_master[:, 2])  # L2
+    assert_array_equal(dpl_pr[:, 3], dpl_master[:, 3])  # L5
+
+    # Test spike type counts
+    spiketype_counts = {}
+    for spikegid in net.spikes.gids[0]:
+        if net.gid_to_type(spikegid) not in spiketype_counts:
+            spiketype_counts[net.gid_to_type(spikegid)] = 0
+        else:
+            spiketype_counts[net.gid_to_type(spikegid)] += 1
+    assert 'common' not in spiketype_counts
+    assert 'exgauss' not in spiketype_counts
+    assert 'extpois' not in spiketype_counts
+    assert spiketype_counts == {'evprox1': 269,
+                                'L2_basket': 54,
+                                'L2_pyramidal': 113,
+                                'L5_pyramidal': 395,
+                                'L5_basket': 85,
+                                'evdist1': 234,
+                                'evprox2': 269}
+
+
+def test_hnn_core():
+    """Test that running hnn-core succeeds (implicit n_jobs=1)."""
+    run_hnn_core(None)
+
+
+def test_mpi():
+    """Test that running hnn-core with MPI succeeds when n_jobs=1."""
+    run_hnn_core('mpi')
+
+    # Test that running hnn-core with MPI fails when n_jobs=2.
+    pytest.raises(ValueError, run_hnn_core, 'mpi', 2)
+
+
+def test_joblib():
+    """Test that running hnn-core with Joblib succeeds when n_jobs=2."""
+    run_hnn_core('joblib', n_jobs=2)
+>>>>>>> TST: add joblib and update MPI
