a45948de886c69247097f830c09a848bde55f1b1
rythorpe <ryan_thorpe@brown.edu>
2020-06-07 19:16:44 -0400

    create spikes class

--- hnn_core/__init__.py
+++ hnn_core/__init__.py
@@ -5,6 +5,6 @@ load_custom_mechanisms()
 from .dipole import simulate_dipole, read_dipole
 from .feed import ExtFeed
 from .params import Params, read_params
-from .network import Network
+from .network import Network, Spikes, read_spikes
 from .pyramidal import L2Pyr, L5Pyr
 from .basket import L2Basket, L5Basket
