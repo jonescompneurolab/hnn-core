c61d265d6adaa6cb15b88d5432e77814419eb31d
Blake Caldwell <blake_caldwell@brown.edu>
2020-07-14 13:55:31 -0400

    MAINT: use _GLOBAL instead of GLOBAL to avoid collisions

--- hnn_core/cell.py
+++ hnn_core/cell.py
@@ -361,9 +361,9 @@ class _Cell(object):
         nc : instance of h.NetCon
             A network connection object.
         """
-        from .neuron import PC
+        from .neuron import _PC
 
-        nc = PC.gid_connect(gid_presyn, postsyn)
+        nc = _PC.gid_connect(gid_presyn, postsyn)
         # calculate distance between cell positions with pardistance()
         d = self._pardistance(nc_dict['pos_src'])
         # set props here
