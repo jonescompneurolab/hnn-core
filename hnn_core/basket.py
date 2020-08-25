5845d1732e24d02aa13912195e84ecf73fc1842b
Mainak Jas <mainakjas@gmail.com>
2020-08-25 00:28:58 -0400

    MAINT: refactor also basket cell

--- hnn_core/basket.py
+++ hnn_core/basket.py
@@ -61,27 +61,6 @@ class BasketSingle(_Cell):
         self.soma_gabaa = self.syn_gabaa_create(self.soma(0.5))
         self.soma_nmda = self.syn_nmda_create(self.soma(0.5))
 
-
-class L2Basket(BasketSingle):
-    """Class for layer 2 basket cells."""
-
-    def __init__(self, gid=-1, pos=-1):
-        # BasketSingle.__init__(self, pos, L, diam, Ra, cm)
-        # Note: Basket cell properties set in BasketSingle())
-        BasketSingle.__init__(self, gid, pos, 'L2Basket')
-        self.celltype = 'L2_basket'
-
-        self._synapse_create()
-        self._biophysics()
-
-    # par connect between all presynaptic cells
-    # no connections from L5Pyr or L5Basket to L2Baskets
-    def parconnect(self, gid, gid_dict, pos_dict, p):
-        self._connect(gid, gid_dict, pos_dict, p, 'L2_pyramidal', 'L2Pyr',
-                      postsyns=[self.soma_ampa])
-        self._connect(gid, gid_dict, pos_dict, p, 'L2_basket', 'L2Basket',
-                      lamtha=20., postsyns=[self.soma_gabaa])
-
     # this function might make more sense as a method of net?
     # par: receive from external inputs
     def parreceive(self, gid, gid_dict, pos_dict, p_ext):
@@ -89,12 +68,12 @@ class L2Basket(BasketSingle):
         for gid_src, p_src, pos in zip(gid_dict['common'],
                                        p_ext, pos_dict['common']):
             # check if AMPA params are defined in the p_src
-            if 'L2Basket_ampa' in p_src.keys():
+            if f'{self.name}_ampa' in p_src.keys():
                 # create an nc_dict
                 nc_dict_ampa = {
                     'pos_src': pos,
-                    'A_weight': p_src['L2Basket_ampa'][0],
-                    'A_delay': p_src['L2Basket_ampa'][1],
+                    'A_weight': p_src[f'{self.name}_ampa'][0],
+                    'A_delay': p_src[f'{self.name}_ampa'][1],
                     'lamtha': p_src['lamtha'],
                     'threshold': p_src['threshold'],
                     'type_src': 'ext'
@@ -105,163 +84,11 @@ class L2Basket(BasketSingle):
                     gid_src, nc_dict_ampa, self.soma_ampa))
 
             # Check if NMDA params are defined in p_src
-            if 'L2Basket_nmda' in p_src.keys():
+            if f'{self.name}_nmda' in p_src.keys():
                 nc_dict_nmda = {
                     'pos_src': pos,
-                    'A_weight': p_src['L2Basket_nmda'][0],
-                    'A_delay': p_src['L2Basket_nmda'][1],
-                    'lamtha': p_src['lamtha'],
-                    'threshold': p_src['threshold'],
-                    'type_src': 'ext'
-                }
-
-                # NMDA synapse
-                self.ncfrom_common.append(self.parconnect_from_src(
-                    gid_src, nc_dict_nmda, self.soma_nmda))
-
-    # one parreceive function to handle all types of external parreceives
-    # types must be defined explicitly here
-    def parreceive_ext(self, type, gid, gid_dict, pos_dict, p_ext):
-        """Receive external inputs."""
-        if type.startswith(('evprox', 'evdist')):
-            if self.celltype in p_ext.keys():
-                gid_ev = gid + gid_dict[type][0]
-
-                nc_dict_ampa = {
-                    'pos_src': pos_dict[type][gid],
-                    # index 0 is ampa weight
-                    'A_weight': p_ext[self.celltype][0],
-                    'A_delay': p_ext[self.celltype][2],  # index 2 is delay
-                    'lamtha': p_ext['lamtha_space'],
-                    'threshold': p_ext['threshold'],
-                    'type_src': type
-                }
-
-                nc_dict_nmda = {
-                    'pos_src': pos_dict[type][gid],
-                    # index 1 is nmda weight
-                    'A_weight': p_ext[self.celltype][1],
-                    'A_delay': p_ext[self.celltype][2],  # index 2 is delay
-                    'lamtha': p_ext['lamtha_space'],
-                    'threshold': p_ext['threshold'],
-                    'type_src': type
-                }
-
-                # connections depend on location of input - why only
-                # for L2 basket and not L5 basket?
-                if p_ext['loc'] in ['proximal', 'distal']:
-                    self.ncfrom_ev.append(self.parconnect_from_src(
-                        gid_ev, nc_dict_ampa, self.soma_ampa))
-                    # NEW: note that default/original is 0 nmda weight for
-                    # the soma (for prox evoked)
-                    self.ncfrom_ev.append(self.parconnect_from_src(
-                        gid_ev, nc_dict_nmda, self.soma_nmda))
-
-        elif type == 'extgauss':
-            # gid is this cell's gid
-            # gid_dict is the whole dictionary, including the gids
-            # of the extgauss
-            # pos_list is also the pos of the extgauss (net origin)
-            # p_ext_gauss are the params (strength, etc.)
-            # I recognize this is ugly (hack)
-            if self.celltype in p_ext.keys():
-                # since gid ids are unique, then these will all be shifted.
-                # if order of extgauss random feeds ever matters (likely)
-                # then will have to preserve order
-                # of creation based on gid ids of the cells
-                # this is a dumb place to put this information
-                gid_extgauss = gid + gid_dict['extgauss'][0]
-
-                # gid works here because there are as many pos
-                # items in pos_dict['extgauss'] as there are cells
-                nc_dict = {
-                    'pos_src': pos_dict['extgauss'][gid],
-                    # index 0 is ampa weight
-                    'A_weight': p_ext[self.celltype][0],
-                    'A_delay': p_ext[self.celltype][1],  # index 2 is delay
-                    'lamtha': p_ext['lamtha'],
-                    'threshold': p_ext['threshold'],
-                    'type_src': type
-                }
-
-                self.ncfrom_extgauss.append(self.parconnect_from_src(
-                    gid_extgauss, nc_dict, self.soma_ampa))
-
-        elif type == 'extpois':
-            if self.celltype in p_ext.keys():
-                gid_extpois = gid + gid_dict['extpois'][0]
-
-                nc_dict = {
-                    'pos_src': pos_dict['extpois'][gid],
-                    # index 0 is ampa weight
-                    'A_weight': p_ext[self.celltype][0],
-                    'A_delay': p_ext[self.celltype][2],  # index 2 is delay
-                    'lamtha': p_ext['lamtha_space'],
-                    'threshold': p_ext['threshold'],
-                    'type_src': type
-                }
-
-                self.ncfrom_extpois.append(self.parconnect_from_src(
-                    gid_extpois, nc_dict, self.soma_ampa))
-
-                if p_ext[self.celltype][1] > 0.0:
-                    # index 1 for nmda weight
-                    nc_dict['A_weight'] = p_ext[self.celltype][1]
-                    self.ncfrom_extpois.append(self.parconnect_from_src(
-                        gid_extpois, nc_dict, self.soma_nmda))
-
-        else:
-            print("Warning, type def not specified in L2Basket")
-
-
-class L5Basket(BasketSingle):
-    """Class for layer 5 basket cells."""
-
-    def __init__(self, gid=-1, pos=-1):
-        # Note: Cell properties are set in BasketSingle()
-        BasketSingle.__init__(self, gid, pos, 'L5Basket')
-        self.celltype = 'L5_basket'
-
-        self._synapse_create()
-        self._biophysics()
-
-    # connections FROM other cells TO this cell
-    # there are no connections from the L2Basket cells. congrats!
-    def parconnect(self, gid, gid_dict, pos_dict, p):
-        self._connect(gid, gid_dict, pos_dict, p, 'L5_basket', 'L5Basket',
-                      lamtha=20., autapses=False,
-                      postsyns=[self.soma_gabaa])
-        self._connect(gid, gid_dict, pos_dict, p, 'L5_pyramidal', 'L5Pyr',
-                      postsyns=[self.soma_ampa])
-        self._connect(gid, gid_dict, pos_dict, p, 'L2_pyramidal', 'L2Pyr',
-                      postsyns=[self.soma_ampa])
-
-    # parallel receive function parreceive()
-    def parreceive(self, gid, gid_dict, pos_dict, p_ext):
-        """Receive."""
-        for gid_src, p_src, pos in zip(gid_dict['common'], p_ext,
-                                       pos_dict['common']):
-            # Check if AMPA params are define in p_src
-            if 'L5Basket_ampa' in p_src.keys():
-                nc_dict_ampa = {
-                    'pos_src': pos,
-                    'A_weight': p_src['L5Basket_ampa'][0],
-                    'A_delay': p_src['L5Basket_ampa'][1],  # right index??
-                    'lamtha': p_src['lamtha'],
-                    'threshold': p_src['threshold'],
-                    'type_src': 'ext'
-                }
-
-                # AMPA synapse
-                self.ncfrom_common.append(self.parconnect_from_src(
-                    gid_src, nc_dict_ampa, self.soma_ampa))
-
-            # Check if nmda params are define in p_src
-            if 'L5Basket_nmda' in p_src.keys():
-                nc_dict_nmda = {
-                    'pos_src': pos,
-                    'A_weight': p_src['L5Basket_nmda'][0],
-                    'A_delay': p_src['L5Basket_nmda'][1],  # right index??
+                    'A_weight': p_src[f'{self.name}_nmda'][0],
+                    'A_delay': p_src[f'{self.name}_nmda'][1],
                     'lamtha': p_src['lamtha'],
                     'threshold': p_src['threshold'],
                     'type_src': 'ext'
@@ -314,14 +141,14 @@ class L5Basket(BasketSingle):
             # of the extgauss
             # pos_dict is also the pos of the extgauss (net origin)
             # p_ext_gauss are the params (strength, etc.)
-            if 'L5_basket' in p_ext.keys():
+            if self.celltype in p_ext.keys():
                 gid_extgauss = gid + gid_dict['extgauss'][0]
 
                 nc_dict = {
                     'pos_src': pos_dict['extgauss'][gid],
                     # index 0 is ampa weight
-                    'A_weight': p_ext['L5_basket'][0],
-                    'A_delay': p_ext['L5_basket'][2],  # index 2 is delay
+                    'A_weight': p_ext[self.celltype][0],
+                    'A_delay': p_ext[self.celltype][2],  # index 2 is delay
                     'lamtha': p_ext['lamtha'],
                     'threshold': p_ext['threshold'],
                     'type_src': type
@@ -355,3 +182,47 @@ class L5Basket(BasketSingle):
 
         else:
             print("Warning, type def not specified in L2Basket")
+
+
+class L2Basket(BasketSingle):
+    """Class for layer 2 basket cells."""
+
+    def __init__(self, gid=-1, pos=-1):
+        # BasketSingle.__init__(self, pos, L, diam, Ra, cm)
+        # Note: Basket cell properties set in BasketSingle())
+        BasketSingle.__init__(self, gid, pos, 'L2Basket')
+        self.celltype = 'L2_basket'
+
+        self._synapse_create()
+        self._biophysics()
+
+    # par connect between all presynaptic cells
+    # no connections from L5Pyr or L5Basket to L2Baskets
+    def parconnect(self, gid, gid_dict, pos_dict, p):
+        self._connect(gid, gid_dict, pos_dict, p, 'L2_pyramidal', 'L2Pyr',
+                      postsyns=[self.soma_ampa])
+        self._connect(gid, gid_dict, pos_dict, p, 'L2_basket', 'L2Basket',
+                      lamtha=20., postsyns=[self.soma_gabaa])
+
+
+class L5Basket(BasketSingle):
+    """Class for layer 5 basket cells."""
+
+    def __init__(self, gid=-1, pos=-1):
+        # Note: Cell properties are set in BasketSingle()
+        BasketSingle.__init__(self, gid, pos, 'L5Basket')
+        self.celltype = 'L5_basket'
+
+        self._synapse_create()
+        self._biophysics()
+
+    # connections FROM other cells TO this cell
+    # there are no connections from the L2Basket cells. congrats!
+    def parconnect(self, gid, gid_dict, pos_dict, p):
+        self._connect(gid, gid_dict, pos_dict, p, 'L5_basket', 'L5Basket',
+                      lamtha=20., autapses=False,
+                      postsyns=[self.soma_gabaa])
+        self._connect(gid, gid_dict, pos_dict, p, 'L5_pyramidal', 'L5Pyr',
+                      postsyns=[self.soma_ampa])
+        self._connect(gid, gid_dict, pos_dict, p, 'L2_pyramidal', 'L2Pyr',
+                      postsyns=[self.soma_ampa])
