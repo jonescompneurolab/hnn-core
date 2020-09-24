7097b52ec1151dfe76e9d586ea1d44feffb4e27d
kohl-carmen <56046017+kohl-carmen@users.noreply.github.com>
2020-09-24 14:41:33 -0400

    changed paths to run on Windows (#189)
    * changed paths to run on Windows
    
    * took out file in err msg
    
    * flake8
    
    * tidying

--- hnn_core/tests/test_dipole.py
+++ hnn_core/tests/test_dipole.py
@@ -12,11 +12,11 @@ from hnn_core.dipole import Dipole, simulate_dipole
 matplotlib.use('agg')
 
 
-def test_dipole():
+def test_dipole(tmpdir):
     """Test dipole object."""
     hnn_core_root = op.dirname(hnn_core.__file__)
     params_fname = op.join(hnn_core_root, 'param', 'default.json')
-    dpl_out_fname = '/tmp/dpl1.txt'
+    dpl_out_fname = tmpdir.join('dpl1.txt')
     params = read_params(params_fname)
     times = np.random.random(6000)
     data = np.random.random((6000, 3))
