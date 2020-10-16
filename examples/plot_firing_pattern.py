47a166afe5668f1f1e6dfaa71bfe2bf30de13aa9
Nick Tolley <nic.m.tolley@gmail.com>
2020-10-16 17:53:34 -0400

    Update firing pattern example with somatic voltage recording functionality

--- examples/plot_firing_pattern.py
+++ examples/plot_firing_pattern.py
@@ -15,8 +15,7 @@ import os.path as op
 # Let us import hnn_core
 
 import hnn_core
-from hnn_core import read_params, Network
-from hnn_core.network_builder import NetworkBuilder
+from hnn_core import read_params, Network, simulate_dipole
 
 hnn_core_root = op.dirname(hnn_core.__file__)
 
@@ -30,26 +29,33 @@ params = read_params(params_fname)
 import matplotlib.pyplot as plt
 
 net = Network(params)
-with NetworkBuilder(net) as network_builder:
-    network_builder.cells[0].plot_voltage()
+dpls = simulate_dipole(net, n_trials=1)
 
-    # The cells are stored in the network object as a list
-    cells = network_builder.cells
-    print(cells[:5])
+# The cell IDs (gids) are stored in the network object as a dictionary
+gid_dict = net.gid_dict
+print(net.gid_dict)
 
-    # We have different kinds of cells with different cell IDs (gids)
-    gids = [0, 35, 135, 170]
-    for gid in gids:
-        print(cells[gid].name)
+# Simulated cell responses are stored in the Spikes object as a dictionary.
+trial_idx = 0
+vsoma = net.spikes.vsoma[trial_idx]
+print(vsoma.keys())
 
-    # We can plot the firing pattern of individual cells
-    network_builder.cells[0].plot_voltage()
-    plt.title('%s (gid=%d)' % (cells[0].name, gid))
+# We can plot the firing pattern of individual cells by indexing with the gid
+gid = 170
+t_vec = net.spikes.t_vec[trial_idx]
+plt.figure(figsize=(4, 4))
+plt.plot(t_vec, vsoma[gid])
+plt.title('%s (gid=%d)' % (net.gid_to_type(gid), gid))
+plt.xlabel('Time (ms)')
+plt.ylabel('Voltage (mV)')
+plt.show()
 
 ###############################################################################
-# Let's do this for the rest of the cell types with a new NetworkBuilder object
-with NetworkBuilder(net) as network_builder:
-    fig, axes = plt.subplots(1, 2, sharey=True, figsize=(8, 4))
-    for gid, ax in zip([35, 170], axes):
-        network_builder.cells[gid].plot_voltage(ax)
-        ax.set_title('%s (gid=%d)' % (cells[gid].name, gid))
+# # Let's do this for the rest of the cell types
+fig, axes = plt.subplots(1, 2, sharey=True, figsize=(8, 4))
+for gid, ax in zip([35, 170], axes):
+    ax.plot(t_vec, vsoma[gid])
+    ax.set_title('%s (gid=%d)' % (net.gid_to_type(gid), gid))
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('Voltage (mV)')
+plt.show()
