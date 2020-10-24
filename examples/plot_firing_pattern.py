cb6f60c21af318b0e23cd2be89388b8b30f10bcc
Nick Tolley <nic.m.tolley@gmail.com>
2020-10-24 18:42:19 -0400

    Add spike raster and histogram to plot_firing_pattern.py

--- examples/plot_firing_pattern.py
+++ examples/plot_firing_pattern.py
@@ -31,15 +31,18 @@ import matplotlib.pyplot as plt
 net = Network(params)
 dpls = simulate_dipole(net, n_trials=1)
 
+###############################################################################
 # The cell IDs (gids) are stored in the network object as a dictionary
 gid_dict = net.gid_dict
 print(net.gid_dict)
 
+###############################################################################
 # Simulated cell responses are stored in the Spikes object as a dictionary.
 trial_idx = 0
 vsoma = net.spikes.vsoma[trial_idx]
 print(vsoma.keys())
 
+###############################################################################
 # We can plot the firing pattern of individual cells by indexing with the gid
 gid = 170
 t_vec = net.spikes.t_vec[trial_idx]
@@ -51,7 +54,7 @@ plt.ylabel('Voltage (mV)')
 plt.show()
 
 ###############################################################################
-# # Let's do this for the rest of the cell types
+# Let's do this for the rest of the cell types
 fig, axes = plt.subplots(1, 2, sharey=True, figsize=(8, 4))
 for gid, ax in zip([35, 170], axes):
     ax.plot(t_vec, vsoma[gid])
@@ -59,3 +62,15 @@ for gid, ax in zip([35, 170], axes):
     ax.set_xlabel('Time (ms)')
     ax.set_ylabel('Voltage (mV)')
 plt.show()
+
+###############################################################################
+# The spiking activity across cell types can also be visualized with raster
+# plots and histograms
+
+# Spike raster plot
+net.spikes.plot()
+
+# Spike histogram
+fig, axes = plt.subplots(1, 1, figsize=(8, 4))
+net.spikes.plot_hist(ax=axes, spike_types=['L2_pyramidal', 'L5_pyramidal'])
+axes.set_xlabel('Time (ms)')
