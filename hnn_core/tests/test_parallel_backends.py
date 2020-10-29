077f87cd8c5293434a51bef9d28d79f1e10d04f2
Mainak Jas <mainakjas@gmail.com>
2020-10-29 15:40:22 -0400

    TST streamline a bit

--- hnn_core/tests/test_parallel_backends.py
+++ hnn_core/tests/test_parallel_backends.py
@@ -14,7 +14,21 @@ from hnn_core import simulate_dipole, Network, read_params
 from hnn_core import MPIBackend, JoblibBackend
 
 
-def run_hnn_core(params, backend=None, n_procs=None, n_jobs=1):
+def run_hnn_core(backend=None, n_procs=None, n_jobs=1, reduced=False):
+    hnn_core_root = op.dirname(hnn_core.__file__)
+
+    # default params
+    params_fname = op.join(hnn_core_root, 'param', 'default.json')
+    params = read_params(params_fname)
+
+    if reduced:
+        params.update({'N_pyr_x': 3,
+                       'N_pyr_y': 3,
+                       'tstop': 25,
+                       't_evprox_1': 5,
+                       't_evdist_1': 10,
+                       't_evprox_2': 20,
+                       'N_trials': 2})
     net = Network(params)
 
     if backend == 'mpi':
@@ -27,72 +41,6 @@ def run_hnn_core(params, backend=None, n_procs=None, n_jobs=1):
         dpls = simulate_dipole(net)
 
     return dpls, net
-
-
-def run_hnn_core_reduced(backend=None, n_procs=None, n_jobs=1):
-    hnn_core_root = op.dirname(hnn_core.__file__)
-
-    # default params
-    params_fname = op.join(hnn_core_root, 'param', 'default.json')
-    params = read_params(params_fname)
-    params_reduced = params.copy()
-    # reduced model params (2 trials)
-    params_reduced.update({'N_pyr_x': 3,
-                           'N_pyr_y': 3,
-                           'tstop': 25,
-                           't_evprox_1': 5,
-                           't_evdist_1': 10,
-                           't_evprox_2': 20,
-                           'N_trials': 2})
-
-    return run_hnn_core(params_reduced, backend=backend, n_procs=n_procs,
-                        n_jobs=n_jobs)
-
-
-def compare_hnn_core(backend=None, n_jobs=1):
-    # small snippet of data on data branch for now. To be deleted
-    # later. Data branch should have only commit so it does not
-    # pollute the history.
-    data_url = ('https://raw.githubusercontent.com/jonescompneurolab/'
-                'hnn-core/test_data/dpl.txt')
-    if not op.exists('dpl.txt'):
-        _fetch_file(data_url, 'dpl.txt')
-    dpl_master = loadtxt('dpl.txt')
-
-    hnn_core_root = op.dirname(hnn_core.__file__)
-
-    # default params
-    params_fname = op.join(hnn_core_root, 'param', 'default.json')
-    params = read_params(params_fname)
-
-    dpls, net = run_hnn_core(params, backend)
-    dpl = dpls[0]
-
-    # write the dipole to a file and compare
-    fname = './dpl2.txt'
-    dpl.write(fname)
-
-    dpl_pr = loadtxt(fname)
-    assert_array_equal(dpl_pr[:, 2], dpl_master[:, 2])  # L2
-    assert_array_equal(dpl_pr[:, 3], dpl_master[:, 3])  # L5
-
-    # Test spike type counts
-    spiketype_counts = {}
-    for spikegid in net.spikes.gids[0]:
-        if net.gid_to_type(spikegid) not in spiketype_counts:
-            spiketype_counts[net.gid_to_type(spikegid)] = 0
-        else:
-            spiketype_counts[net.gid_to_type(spikegid)] += 1
-    assert 'common' not in spiketype_counts
-    assert 'exgauss' not in spiketype_counts
-    assert 'extpois' not in spiketype_counts
-    assert spiketype_counts == {'evprox1': 269,
-                                'L2_basket': 54,
-                                'L2_pyramidal': 113,
-                                'L5_pyramidal': 395,
-                                'L5_basket': 85,
-                                'evdist1': 234,
-                                'evprox2': 269}
 
 
 # The purpose of this incremental mark is to avoid running the full length
@@ -110,7 +58,7 @@ class TestParallelBackends():
     def test_run_default(self):
         """Test consistency between default backend simulation and master"""
         global dpls_reduced_default
-        dpls_reduced_default, _ = run_hnn_core_reduced(None)
+        dpls_reduced_default, _ = run_hnn_core(None, reduced=True)
         # test consistency across all parallel backends for multiple trials
         assert_raises(AssertionError, assert_array_equal,
                       dpls_reduced_default[0].data['agg'],
@@ -120,8 +68,8 @@ class TestParallelBackends():
         """Test consistency between joblib backend simulation with master"""
         global dpls_reduced_default, dpls_reduced_joblib
 
-        dpls_reduced_joblib, _ = run_hnn_core_reduced(backend='joblib',
-                                                      n_jobs=2)
+        dpls_reduced_joblib, _ = run_hnn_core(backend='joblib',
+                                              n_jobs=2, reduced=True)
 
         for trial_idx in range(len(dpls_reduced_default)):
             assert_array_equal(dpls_reduced_default[trial_idx].data['agg'],
@@ -140,7 +88,7 @@ class TestParallelBackends():
         """Test running a MPIBackend on reduced model"""
         global dpls_reduced_default, dpls_reduced_mpi
         pytest.importorskip("mpi4py", reason="mpi4py not available")
-        dpls_reduced_mpi, _ = run_hnn_core_reduced(backend='mpi')
+        dpls_reduced_mpi, _ = run_hnn_core(backend='mpi', reduced=True)
         for trial_idx in range(len(dpls_reduced_default)):
             # account for rounding error incured during MPI parallelization
             assert_allclose(dpls_reduced_default[trial_idx].data['agg'],
@@ -152,13 +100,54 @@ class TestParallelBackends():
         pytest.importorskip("mpi4py", reason="mpi4py not available")
 
         oversubscribed = cpu_count() * 1
-        run_hnn_core_reduced(backend='mpi', n_procs=oversubscribed)
+        run_hnn_core(backend='mpi', n_procs=oversubscribed, reduced=True)
 
-    def test_compare_hnn_core(self):
+    @pytest.mark.parametrize("backend", ['mpi', 'joblib'])
+    def test_compare_hnn_core(self, backend, n_jobs=1):
         """Test hnn-core does not break."""
-        # run one trial of each
-        compare_hnn_core(backend='mpi')
-        compare_hnn_core(backend='joblib')
+        # small snippet of data on data branch for now. To be deleted
+        # later. Data branch should have only commit so it does not
+        # pollute the history.
+        data_url = ('https://raw.githubusercontent.com/jonescompneurolab/'
+                    'hnn-core/test_data/dpl.txt')
+        if not op.exists('dpl.txt'):
+            _fetch_file(data_url, 'dpl.txt')
+        dpl_master = loadtxt('dpl.txt')
+
+        hnn_core_root = op.dirname(hnn_core.__file__)
+
+        # default params
+        params_fname = op.join(hnn_core_root, 'param', 'default.json')
+        params = read_params(params_fname)
+
+        dpls, net = run_hnn_core(params, backend)
+        dpl = dpls[0]
+
+        # write the dipole to a file and compare
+        fname = './dpl2.txt'
+        dpl.write(fname)
+
+        dpl_pr = loadtxt(fname)
+        assert_array_equal(dpl_pr[:, 2], dpl_master[:, 2])  # L2
+        assert_array_equal(dpl_pr[:, 3], dpl_master[:, 3])  # L5
+
+        # Test spike type counts
+        spiketype_counts = {}
+        for spikegid in net.spikes.gids[0]:
+            if net.gid_to_type(spikegid) not in spiketype_counts:
+                spiketype_counts[net.gid_to_type(spikegid)] = 0
+            else:
+                spiketype_counts[net.gid_to_type(spikegid)] += 1
+        assert 'common' not in spiketype_counts
+        assert 'exgauss' not in spiketype_counts
+        assert 'extpois' not in spiketype_counts
+        assert spiketype_counts == {'evprox1': 269,
+                                    'L2_basket': 54,
+                                    'L2_pyramidal': 113,
+                                    'L5_pyramidal': 395,
+                                    'L5_basket': 85,
+                                    'evdist1': 234,
+                                    'evprox2': 269}
 
 
 # there are no dependencies if this unit tests fails; no need to be in
@@ -173,7 +162,7 @@ def test_mpi_failure():
     with pytest.warns(UserWarning) as record:
         with io.StringIO() as buf, redirect_stdout(buf):
             with pytest.raises(RuntimeError, match="MPI simulation failed"):
-                run_hnn_core_reduced(backend='mpi')
+                run_hnn_core(backend='mpi', reduced=True)
             stdout = buf.getvalue()
 
     assert "MPI processes are unable to reach each other" in stdout
