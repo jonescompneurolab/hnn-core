93733560ee87cddc1b448baf37070ee243138d75
Nick Tolley <nic.m.tolley@gmail.com>
2020-11-21 12:19:28 -0500

    Change Spikes class to CellResponse

--- hnn_core/__init__.py
+++ hnn_core/__init__.py
@@ -1,7 +1,7 @@
 from .dipole import simulate_dipole, read_dipole, average_dipoles
 from .feed import feed_event_times
 from .params import Params, read_params
-from .network import Network, Spikes, read_spikes
+from .network import Network, CellResponse, read_spikes
 from .pyramidal import L2Pyr, L5Pyr
 from .basket import L2Basket, L5Basket
 from .parallel_backends import MPIBackend, JoblibBackend
