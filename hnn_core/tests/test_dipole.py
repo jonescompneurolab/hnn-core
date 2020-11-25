affae28943580d6adeef332d2ef8b6b39587ce87
Mainak Jas <mainakjas@gmail.com>
2020-11-25 17:45:20 -0500

    MAINT rebase

--- hnn_core/tests/test_dipole.py
+++ hnn_core/tests/test_dipole.py
@@ -121,5 +121,3 @@ def test_cell_response_backends(run_hnn_core_fixture):
     assert np.all([spike_times[spike_gids == gid] > times[v_mask][0],
                    spike_times[spike_gids == gid] < times[v_mask][-1]])
     simulate_dipole(net, n_trials=2, record_vsoma=True)
-    assert len(net.spikes.vsoma) == 2
-    assert len(net.spikes.vsoma[0]) == 24
