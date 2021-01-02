76b988704701d52c4597c49ce957bd852ea31eac
Christopher J. Bailey <bailey.cj@gmail.com>
2021-01-02 16:03:22 +0100

    ENH alpha example replicates online docs

--- examples/plot_simulate_alpha.py
+++ examples/plot_simulate_alpha.py
@@ -29,32 +29,33 @@ params = read_params(params_fname)
 print(params)
 
 ###############################################################################
-# Remove all evoked proximal feed parameters
-for key in params['*evprox*']:
-    del params[key]
-
-# Remove all evoked distal feed parameters
-for key in params['*evdist*']:
-    del params[key]
-
-###############################################################################
-# Now, we update a few rhythmic feed parameters
+# Update a few of the default parameters related to visualisation
 params.update({
     'dipole_scalefctr': 150000.0,
     'dipole_smooth_win': 0,
+    'sync_evinput': 1,  # XXX not applied (seed hack, see feed.py:_get_prng)
     'tstop': 310.0,
-    't0_input_dist': 50.0,
-    'tstop_input_dist': 1001.0,
-    'input_dist_A_weight_L2Pyr_ampa': 5.4e-5,
-    'input_dist_A_weight_L5Pyr_ampa': 5.4e-5,
-    'sync_evinput': 1,
-    "prng_seedcore_input_dist": 3
 })
 
 ###############################################################################
 # Now let's simulate the dipole and plot it
 net = Network(params)
-dpl = simulate_dipole(net)
+
+# XXX to match online docs, remove before MRG
+net.add_bursty_drive(
+    'bogus', distribution='normal', t0=50., sigma_t0=0., T=params['tstop'],
+    burst_f=10, spike_jitter_std=20., numspikes=2, spike_isi=10, repeats=10,
+    weights_ampa=None, weights_nmda=None, location='proximal',
+    seedcore=3, space_constant=100.)
+
+weights_ampa = {'L2_pyramidal': 5.4e-5, 'L5_pyramidal': 5.4e-5}
+net.add_bursty_drive(
+    'bursty', distribution='normal', t0=50., sigma_t0=0., T=params['tstop'],
+    burst_f=10, spike_jitter_std=20., numspikes=2, spike_isi=10, repeats=10,
+    weights_ampa=weights_ampa, weights_nmda=None, location='distal',
+    seedcore=3, space_constant=100.)
+
+dpl = simulate_dipole(net, n_trials=1)  # XXX n_trials=1 instantiates drive!
 dpl[0].plot()
 
 ###############################################################################
