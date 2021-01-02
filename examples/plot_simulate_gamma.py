521ecb92b11709029e445d3b37399dfd927b033a
Christopher J. Bailey <bailey.cj@gmail.com>
2021-01-02 17:24:25 +0100

    ENH gamma simulation with single Poisson drive working

--- examples/plot_simulate_gamma.py
+++ examples/plot_simulate_gamma.py
@@ -31,6 +31,20 @@ print(params)
 # Now let's simulate the dipole
 
 net = Network(params)
+
+# add a tonic Poisson-distributed excitation to pyramidal cells
+weights_ampa = {'L2_pyramidal': 0.0008, 'L5_pyramidal': 0.0075}
+dispersion_time = {'L2_pyramidal': 0.1, 'L5_pyramidal': 1.0}
+rate_constants = {'L2_pyramidal': 140.0, 'L5_pyramidal': 40.0}
+# XXX online docs had a seed of -3 (!). This worked because extpois was added
+# last, the first artificial cell having gid=1352, leading to seeds >= 1349
+# With the new API, seeds begin from 270, so init_seed = 1352 - 3 - 270
+prng_initial_seed = 1079  # XXX fubar, but will work because gids are >> 0
+net.add_poisson_drive(
+    'poisson', rate_constants=rate_constants, weights_ampa=weights_ampa,
+    location='proximal', dispersion_time=dispersion_time,
+    seedcore=prng_initial_seed)
+
 dpls = simulate_dipole(net, n_trials=1)
 
 ###############################################################################
