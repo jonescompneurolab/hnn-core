e4bc43dd7350ca4cefe4dc97ab6a332814b35abd
Nick Tolley <nic.m.tolley@gmail.com>
2021-01-22 17:50:19 -0500

    Add proximal drive to alpha example to simulate beta events

--- examples/plot_simulate_alpha.py
+++ examples/plot_simulate_alpha.py
@@ -1,14 +1,15 @@
 """
-========================
-02. Simulate alpha waves
-========================
+=================================
+02. Simulate alpha and beta waves
+=================================
 
-This example demonstrates how to simulate alpha waves using
+This example demonstrates how to simulate alpha and beta waves using
 HNN-core.
 """
 
 # Authors: Mainak Jas <mainak.jas@telecom-paristech.fr>
 #          Sam Neymotin <samnemo@gmail.com>
+#          Nick Tolley <nicholas_tolley@brown.edu>
 
 import os.path as op
 
@@ -44,11 +45,15 @@ params.update({
 # (dendritic) AMPA synapses on L2/3 and L5 pyramidal neurons.
 net = Network(params)
 
-weights_ampa = {'L2_pyramidal': 5.4e-5, 'L5_pyramidal': 5.4e-5}
+location = 'distal'
+burst_std = 20
+weights_ampa_d = {'L2_pyramidal': 5.4e-5, 'L5_pyramidal': 5.4e-5}
+syn_delays_d = {'L2_basket': 5., 'L2_pyramidal': 5.,
+                'L5_basket': 5., 'L5_pyramidal': 5.}
 net.add_bursty_drive(
-    'bursty', tstart=50., burst_rate=10, burst_std=20., numspikes=2,
-    spike_isi=10, repeats=10, location='distal', weights_ampa=weights_ampa,
-    seedcore=4)
+    'alpha_dist', tstart=50., burst_rate=10, burst_std=burst_std, numspikes=2,
+    spike_isi=10, repeats=10, location=location, weights_ampa=weights_ampa_d,
+    synaptic_delays=syn_delays_d, seedcore=14)
 
 dpl = simulate_dipole(net)
 
@@ -77,7 +82,7 @@ freqs, _, psds = spectrogram(
     nperseg=n_fft, noverlap=0)
 
 ###############################################################################
-# Finally we plot the PSD and confirm the presence of alpha activity from the
+# We can plot the PSD and confirm the presence of alpha activity from the
 # sharp peak centered at 10 Hz.
 plt.figure()
 plt.plot(freqs, np.mean(psds, axis=-1))
@@ -85,3 +90,34 @@ plt.xlim((0, 40))
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('PSD')
 plt.show()
+
+###############################################################################
+# The next step is to add a simultaneous 10 Hz proximal drive. Due to the
+# stochasticity of input spike timing, the proximal and distal spikes
+# occasionally arrive at the same time which will result in a beta frequency
+# (15-30 Hz) event.
+location = 'proximal'
+burst_std = 20
+weights_ampa_p = {'L2_pyramidal': 5.4e-5, 'L5_pyramidal': 5.4e-5}
+syn_delays_p = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                'L5_basket': 1., 'L5_pyramidal': 1.}
+
+net.add_bursty_drive(
+    'alpha_prox', tstart=50., burst_rate=10, burst_std=burst_std, numspikes=2,
+    spike_isi=10, repeats=10, location=location, weights_ampa=weights_ampa_p,
+    synaptic_delays=syn_delays_p, seedcore=13)
+
+dpl = simulate_dipole(net)
+
+###############################################################################
+# It can be difficult to identify beta activity by inspecting the dipole
+# directly. One useful tool is to plot the time frequency spectrogram.
+# Create an fixed-step tiling of frequencies from 20 to 100 Hz in steps of 1 Hz
+from hnn_core.viz import plot_dipole, plot_tfr_morlet
+fig, axes = plt.subplots(2, 1, sharex=True, figsize=(6, 6))
+
+plot_dipole(dpls[trial_idx], ax=axes[0], show=False)
+
+
+freqs = np.arange(2., 50., 1.)
+plot_tfr_morlet(dpls[trial_idx], freqs=freqs, n_cycles=7, ax=axes[1])
