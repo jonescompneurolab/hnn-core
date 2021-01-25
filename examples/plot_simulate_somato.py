cfb66d921c93c4dbe8c661a1351e59262f4a2228
rythorpe <ryan_thorpe@brown.edu>
2021-01-25 18:06:59 -0500

    update drives

--- examples/plot_simulate_somato.py
+++ examples/plot_simulate_somato.py
@@ -38,24 +38,22 @@ fwd_fname = op.join(data_path, 'derivatives', 'sub-{}'.format(subject),
 subjects_dir = op.join(data_path, 'derivatives', 'freesurfer', 'subjects')
 
 ###############################################################################
-# Then, we get the raw data and estimate the inverse operator. This includes
-# reading and band-pass filtering the raw data,
+# Then, we get the raw data and estimate the inverse operator.
+
+# Read and band-pass filter the raw data
 raw = mne.io.read_raw_fif(raw_fname, preload=True)
 raw.filter(1, 40)
 
-###############################################################################
-# isolating events associated with each MEG time series in the dataset,
+# Identify stimulus events associated with MEG time series in the dataset
 events = mne.find_events(raw, stim_channel='STI 014')
 
-###############################################################################
-# defining epochs within the time series,
+# Define epochs within the time series
 event_id, tmin, tmax = 1, -.2, .17
 baseline = None
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline=baseline,
                     reject=dict(grad=4000e-13, eog=350e-6), preload=True)
 
-###############################################################################
-# and finally computing the inverse operator.
+# Compute the inverse operator
 fwd = mne.read_forward_solution(fwd_fname)
 cov = mne.compute_covariance(epochs)
 inv = make_inverse_operator(epochs.info, fwd, cov)
@@ -69,7 +67,6 @@ inv = make_inverse_operator(epochs.info, fwd, cov)
 # to note that the dipole currents simulated with HNN are assumed to be normal
 # to the cortical surface. Hence, using the option ``pick_ori='normal'``
 # seems to make most sense.
-
 method = "MNE"
 snr = 3.
 lambda2 = 1. / snr ** 2
@@ -83,6 +80,7 @@ stc = apply_inverse(evoked, inv, lambda2, method=method, pick_ori="normal",
 # each vertex. The time course from the vertex with the greatest L2 norm
 # represents the location of cortex with greatest response to stimulus.
 pick_vertex = np.argmax(np.linalg.norm(stc.data, axis=1))
+
 plt.figure()
 plt.plot(1e3 * stc.times, stc.data[pick_vertex, :].T * 1e9, 'ro-')
 plt.xlabel('time (ms)')
@@ -93,8 +91,7 @@ plt.show()
 
 ###############################################################################
 # Now, let us try to simulate the same with hnn-core. We read in the network
-# parameters from ``N20.json`` and explicitly create two distal and one
-# proximal evoked drive.
+# parameters from ``N20.json`` and instantiate the network.
 
 import hnn_core
 from hnn_core import simulate_dipole, read_params, Network, MPIBackend, average_dipoles
@@ -106,39 +103,57 @@ params = read_params(params_fname)
 
 net = Network(params)
 
-# Distal evoked drives share connection parameters
+###############################################################################
+# To simulate the source of the median nerve evoked response, we create a
+# sequence of synchronous evoked drives: 1 proximal, 2 distal, and 1 final
+# proximal drive. Note that setting ``sync_within_trial=True`` creates drives
+# with synchronous input (arriving to and transmitted by hypothetical granular
+# cells at the center of the network) to all pyramidal and basket cells that
+# receive distal drive.
+
+# Proximal drives share connection parameters
+weights_ampa_p = {'L2_basket': 0.003, 'L2_pyramidal': 0.0025,
+                  'L5_basket': 0.004, 'L5_pyramidal': 0.001}
+weights_nmda_p = {'L2_basket': 0.003, 'L5_basket': 0.004}
+synaptic_delays_p = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                     'L5_basket': 1.0, 'L5_pyramidal': 1.0}
+
+# Early proximal drive
+net.add_evoked_drive(
+    'evprox1', mu=20.0, sigma=3., numspikes=1, sync_within_trial=True,
+    weights_ampa=weights_ampa_p, weights_nmda=weights_nmda_p,
+    location='proximal', synaptic_delays=synaptic_delays_p, seedcore=6)
+
+# Late proximal drive
+net.add_evoked_drive(
+    'evprox2', mu=130.0, sigma=3., numspikes=1, sync_within_trial=True,
+    weights_ampa=weights_ampa_p, weights_nmda=weights_nmda_p,
+    location='proximal', synaptic_delays=synaptic_delays_p, seedcore=6)
+
+# Early distal drive
 weights_ampa_d = {'L2_basket': 0.003, 'L2_pyramidal': 0.0045,
                   'L5_pyramidal': 0.001}
 weights_nmda_d = {'L2_basket': 0.003, 'L2_pyramidal': 0.0045,
                   'L5_pyramidal': 0.001}
 synaptic_delays_d = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
                      'L5_pyramidal': 0.1}
-# early distal input
+
+# Late distal drive
 net.add_evoked_drive(
     'evdist1', mu=32., sigma=3., numspikes=1, sync_within_trial=True,
     weights_ampa=weights_ampa_d, weights_nmda=weights_nmda_d,
     location='distal', synaptic_delays=synaptic_delays_d, seedcore=6)
-# late distal input
+
+# Late distal input
 net.add_evoked_drive(
     'evdist2', mu=82., sigma=3., numspikes=1, sync_within_trial=True,
     weights_ampa=weights_ampa_d, weights_nmda=weights_nmda_d,
     location='distal', synaptic_delays=synaptic_delays_d, seedcore=2)
 
-# proximal input occurs before distals
-weights_ampa_p = {'L2_basket': 0.003, 'L2_pyramidal': 0.0025,
-                  'L5_basket': 0.004, 'L5_pyramidal': 0.001}
-weights_nmda_p = {'L2_basket': 0.003, 'L5_basket': 0.004}
-synaptic_delays_p = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
-                     'L5_basket': 1.0, 'L5_pyramidal': 1.0}
-net.add_evoked_drive(
-    'evprox1', mu=20.0, sigma=3., numspikes=1, sync_within_trial=True,
-    weights_ampa=weights_ampa_p, weights_nmda=weights_nmda_p,
-    location='proximal', synaptic_delays=synaptic_delays_p, seedcore=6)
-
 # n_trials = 25
-n_trials = 1
+n_trials = 2
 with MPIBackend(n_procs=6, mpi_cmd='mpiexec'):
-    dpls = simulate_dipole(net, n_trials=25)
+    dpls = simulate_dipole(net, n_trials=n_trials)
 
 fig, axes = plt.subplots(3, 1, sharex=True, figsize=(6, 6))
 axes[1].plot(1e3 * stc.times, stc.data[pick_vertex, :].T * 1e9, 'r-')
