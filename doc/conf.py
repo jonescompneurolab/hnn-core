84bce210aeb6b9dce12373e0fa7ce435f0f32193
rythorpe <ryan_thorpe@brown.edu>
2021-04-04 23:51:29 -0400

    update to dev version

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ copyright = '2021, HNN Developers'
 author = 'HNN Developers'
 
 # The short X.Y version
-version = '0.1'
+version = '0.2.dev0'
 # The full version, including alpha/beta/rc tags
 release = ''
 
