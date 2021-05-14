28e4f70ac3a034be89029d617355dc52c865346f
Christopher J. Bailey <bailey.cj@gmail.com>
2021-05-14 07:53:43 +0200

    squash of several circling commits
    clarify code in lfp.py and fix LSA bug
    
    FIX LSA implementation and test against PSA
    
    address review comments from jasmainak
    
    use nrn Vector object for xfer resistance
    
    ENH imem current handler for clarity and speed
    
    address comments from ntolley
    
    FIX set CVODE.use_fast_imem only if needed
    
    TST move LFP tests from test_dipole to test_lft
    
    REVERT to each _LFPElectrode keeping track of imem
    
    If imem-currents are kept track of by a separate object ("handler"),
    segmentation faults would occur at random, but consistently on the
    Nth (N > 1) trial. The problem was not debuggable, so I decided to
    revert to this working state. It is slightly optimised in terms of:
    
    - calculations are kept in C by using the h.Vector().dot method
    - the time is no longer recorded inside each electrode (redundant)
    
    BUG fix plot times in LFP example
    
    DEMO laminar LFP visualisation for gamma-example
    
    WIP simplify example
    
    fix test using default_network

--- examples/plot_simulate_lfp.py
+++ examples/plot_simulate_lfp.py
@@ -8,6 +8,7 @@ This example demonstrates how to record local field potentials (LFPs).
 
 # Authors: Nick Tolley <nicholas_tolley@brown.edu>
 #          Mainak Jas <mainakjas@gmail.com>
+#          Christopher Bailey <cjb@cfin.au.dk>
 
 # sphinx_gallery_thumbnail_number = 3
 
@@ -18,21 +19,29 @@ import os.path as op
 
 import hnn_core
 from hnn_core import read_params, Network, simulate_dipole
+from hnn_core.parallel_backends import MPIBackend
 
 hnn_core_root = op.dirname(hnn_core.__file__)
 
 ###############################################################################
 # Then we read the parameters file
-params_fname = op.join(hnn_core_root, 'param', 'default.json')
+# params_fname = op.join(hnn_core_root, 'param', 'default.json')
+params_fname = op.join(hnn_core_root, 'param', 'gamma_L5weak_L2weak.json')
 params = read_params(params_fname)
-
+params['tstop'] = 250
 ###############################################################################
 # We will start with simulating the evoked response from
 # :ref:`evoked example <sphx_glr_auto_examples_plot_simulate_evoked.py>`.
 # We first instantiate the network.
 # (Note: Setting ``add_drives_from_params=True`` loads a set of predefined
 # drives without the drives API shown previously).
-net = Network(params, add_drives_from_params=True)
+net = Network(params)
+weights_ampa = {'L2_pyramidal': 0.0008, 'L5_pyramidal': 0.0075}
+synaptic_delays = {'L2_pyramidal': 0.1, 'L5_pyramidal': 1.0}
+rate_constant = {'L2_pyramidal': 140.0, 'L5_pyramidal': 40.0}
+net.add_poisson_drive(
+    'poisson', rate_constant=rate_constant, weights_ampa=weights_ampa,
+    location='proximal', synaptic_delays=synaptic_delays, seedcore=1079)
 
 ###############################################################################
 # LFP recordings require specifying the electrode postion. It can be useful
@@ -46,23 +55,86 @@ net.plot_cells()
 # add them to the simulation.
 
 # XXX coordinates FUBAR, cortical depth direction is Y in NEURON objects!
-electrode_pos = [(2, 400, 2), (6, 800, 6)]
+# electrode_pos = [(2, 400, 2), (6, 800, 6)]
+depths = list(range(-400, 1900, 200))
+electrode_pos = [(45, dep, 45) for dep in depths]
 net.add_electrode(electrode_pos)
 print(net.lfp)
 net.plot_cells()
 
+with MPIBackend(n_procs=2):
+    dpl = simulate_dipole(net)
 ###############################################################################
 # Now that our electrodes are specified, we can run the simulation. The LFP
 # recordings are also stored under ``Network.lfp``.
 import matplotlib.pyplot as plt
 
-dpl = simulate_dipole(net)
-times = dpl[0].times[:-1]
 plt.figure()
 trial_idx = 0
-plt.plot(times, net.lfp[0]['data'][trial_idx])
-plt.plot(times, net.lfp[1]['data'][trial_idx])
-plt.legend([f'e_pos {electrode_pos[0]}', f'e_pos {electrode_pos[1]}'])
+plt.plot(dpl[trial_idx].times, net.lfp[1]['data'][trial_idx])
+plt.plot(dpl[trial_idx].times, net.lfp[-2]['data'][trial_idx])
+plt.legend([f'e_pos {electrode_pos[1]}', f'e_pos {electrode_pos[-2]}'])
 plt.xlabel('Time (ms)')
 plt.ylabel(r'Potential ($\mu V$)')
+plt.show()
+
+from hnn_core.viz import _decimate_plot_data
+from hnn_core.dipole import _hammfilt
+# from matplotlib.colors import SymLogNorm
+# from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+import numpy as np
+window_len = 10  # ms
+decimate = [5, 4]  # from 40k to 8k to 2k
+winsz = np.round(1e-3 * window_len * dpl[trial_idx].sfreq)
+
+plot_data = list()
+for elec in net.lfp:
+    filt_data = _hammfilt(elec['data'][trial_idx], winsz)
+    decim_data, decim_time, lfp_sfreq = _decimate_plot_data(
+        decimate, filt_data, dpl[trial_idx].times, sfreq=dpl[trial_idx].sfreq)
+    plot_data.append(decim_data)
+plot_data = np.array(plot_data)
+
+# fig, axs = plt.subplots(3, 1, sharex=True, figsize=(8, 8),
+#                         gridspec_kw={'height_ratios': [2, 1, 2]})
+# im = axs[0].pcolormesh(decim_time, depths, plot_data,
+#                        norm=SymLogNorm(linthresh=1e-1, linscale=1.,
+#                                        vmin=-10e2, vmax=10e2, base=10),
+#                        cmap='BrBG_r', shading='auto')
+# axs[0].set_ylabel(r'Distance from soma in Y ($\mu m$)')
+# axs[0].set_xticks(np.arange(10, 250, 10), minor=True)
+# axs[0].grid(True, which='both', axis='x')
+# axins = inset_axes(axs[0],
+#                    width="2%",  # width = 5% of parent_bbox width
+#                    height="80%",  # height : 50%
+#                    loc='lower left',
+#                    bbox_to_anchor=(1.075, 0.1, 1, 1),
+#                    bbox_transform=axs[0].transAxes,
+#                    borderpad=0,
+#                    )
+# cbh = fig.colorbar(im, cax=axins, extend='both')
+# cbh.ax.yaxis.set_ticks_position('left')
+# cbh.ax.set_ylabel(r'Potential ($\mu V$)')
+
+# dpl[trial_idx].plot(ax=axs[1], show=False)
+# axs[1].set_xticks(np.arange(10, 250, 10), minor=True)
+# axs[1].grid(True, which='both', axis='x')
+# axs[1].set_xlabel('')
+# axs[1].set_title('')
+
+# net.cell_response.plot_spikes_raster(ax=axs[2], show=False)
+# axs[2].set_xlabel('Time (ms)')
+# plt.show()
+
+show_electrode = 6
+el_dep = depths[show_electrode]
+fig, ax = plt.subplots(1, 1, sharex=True, figsize=(8, 6))
+lfp_ax = ax.twinx()
+dpl[trial_idx].copy().smooth(window_len=window_len).plot(ax=ax, show=False)
+ax.set_title(f'Gamma oscillations in the dipole and LFP waveforms ({el_dep} '
+             r'$\mu m$ above soma)')
+ax.yaxis.label.set_color('#1f77b4')
+lfp_ax.plot(decim_time, plot_data[show_electrode], color='#ff7f0e')
+lfp_ax.set_ylabel(r'Electric potential($\mu V$)', color='#ff7f0e')
 plt.show()
--- hnn_core/lfp.py
+++ hnn_core/lfp.py
@@ -1,34 +1,24 @@
 """
-LFPsim - Simulation scripts to compute Local Field Potentials (LFP) from cable
-compartmental models of neurons and networks implemented in NEURON simulation
-environment.
+Handler classes to calculate Local Field Potentials (LFP) at ideal (point-like)
+electrodes based on net transmembrane currents of all neurons in the network.
 
-LFPsim works reliably on biophysically detailed multi-compartmental neurons
-with ion channels in some or all compartments.
-
-Last updated 12-March-2016
-Developed by :
-Harilal Parasuram & Shyam Diwakar
-Computational Neuroscience & Neurophysiology Lab, School of Biotechnology,
-Amrita University, India.
-
-Email: harilalp@am.amrita.edu; shyam@amrita.edu
-www.amrita.edu/compneuro
-
-translated to Python and modified to use use_fast_imem by Sam Neymotin
-based on mhines code
+The code is inspired by [1], but important modifications were made to comply
+with the original derivation of the 'line source approximation method'.
+LFPsim - Simulation scripts to compute Local Field Potentials (LFP) fro
 
 References
 ----------
 1 . Parasuram H, Nair B, D'Angelo E, Hines M, Naldi G, Diwakar S (2016)
-Computational Modeling of Single Neuron Extracellular Electric Potentials
-and Network Local Field Potentials using LFPsim. Front Comput Neurosci 10:65
-[PubMed].
+Computational Modeling of Single Neuron Extracellular Electric Potentials and
+Network Local Field Potentials using LFPsim. Front Comput Neurosci 10:65.
+2. Holt, G. R. (1998) A critical reexamination of some assumptions and
+implications of cable theory in neurobiology. CalTech, PhD Thesis.
 """
 
 # Authors: Mainak Jas <mjas@mgh.harvard.edu>
 #          Nick Tolley <nicholas_tolley@brown.edu>
 #          Sam Neymotin <samnemo@gmail.com>
+#          Christopher Bailey <cjb@cfin.au.dk>
 
 from neuron import h
 
@@ -36,21 +26,21 @@ import numpy as np
 from numpy.linalg import norm
 
 
-def _get_all_sections(sec_type='Pyr'):
+def _get_sections_on_this_rank(sec_type='Pyr'):
     ls = h.allsec()
     ls = [s for s in ls if sec_type in s.name()]
     return ls
 
 
 def _get_segment_counts(all_sections):
-    '''The segment count of a section excludes the endpoints (0, 1)'''
+    """The segment count of a section excludes the endpoints (0, 1)"""
     seg_counts = list()
     for sec in all_sections:
         seg_counts.append(sec.nseg)
     return seg_counts
 
 
-def _transfer_resistance(section, ele_pos, sigma, method):
+def _transfer_resistance(section, electrode_pos, sigma, method):
     """Transfer resistance between section and electrode position.
 
     To arrive at the extracellular potential, the value returned by this
@@ -60,25 +50,34 @@ def _transfer_resistance(section, ele_pos, sigma, method):
 
     Parameters
     ----------
-    section : h.Section() The NEURON section. ele_pos : list (x, y, z) The x,
-        y, z coordinates of the electrode (in um) sigma : float Extracellular
-        conductivity (in S/m) method : str Approximation to use. 'psa' assigns
-        all transmembrane currents to the center point (0.5). 'lsa' treats the
+    section : h.Section()
+        The NEURON section.
+    ele_pos : list (x, y, z)
+        The x, y, z coordinates of the electrode (in um)
+    sigma : float
+        Extracellular conductivity (in S/m)
+    method : str
+        Approximation to use. 'psa' assigns all transmembrane currents to the
+        center point (0.5) (point source approximation). 'lsa' treats the
         section as a line source, but a single multiplier is calculated for
-        each section. Returns
+        each section (line source approximation).
+
+    Returns
     -------
-    vres : list The resistance at each section.
+    vres : list
+        The resistance at each section.
     """
-    ele_pos = np.array(ele_pos)  # electrode position to Numpy
+    electrode_pos = np.array(electrode_pos)  # electrode position to Numpy
 
     start = np.array([section.x3d(0), section.y3d(0), section.z3d(0)])
     end = np.array([section.x3d(1), section.y3d(1), section.z3d(1)])
-    mid = (start + end) / 2.
 
     if method == 'psa':
 
-        # distance from compartment to electrode
-        dis = norm(ele_pos - mid)
+        mid = (start + end) / 2.
+
+        # distance from section midpoint to electrode
+        dis = norm(electrode_pos - mid)
 
         # setting radius limit
         if dis < section.diam / 2.0:
@@ -86,41 +85,46 @@ def _transfer_resistance(section, ele_pos, sigma, method):
 
         phi = 1. / dis
 
-    # XXX the 'lsa' method implementation is unverified, proceed with caution!
     elif method == 'lsa':
-        # calculate length of the compartment
-        a = end - start
-        dis = norm(a)
-
-        # setting radius limit
-        if dis < section.diam / 2.0:
-            dis = section.diam / 2.0 + 0.1
-
-        # if a = position vector of end with respect to start
-        #    b = position vector of electrode with respect to end
+        # From: Appendix C (pp. 137) in Holt, G. R. A critical reexamination of
+        # some assumptions and implications of cable theory in neurobiology.
+        # CalTech, PhD Thesis (1998).
         #
-        # we want to compute the length of projection of "a"
-        # "b".
-        # H = a.cos(theta) = a.dot(b) / |a|
+        #                      Electrode position
+        #   |------ L --------*
+        #                 b / | R
+        #                 /   |
+        #   0==== a ====1- H -+
+        #
+        # a: vector oriented along the section
+        # b: position vector of electrode with respect to section end (1)
+        # H: parallel distance from section end to electrode
+        # R: radial distance from section end to electrode
+        # L: parallel distance from section start to electrode
+        # Note that there are three distinct regimes to this approximation,
+        # depending on the electrode position along the section axis.
         a = end - start
-        b = ele_pos - end
-        H = a.dot(b) / dis
+        norm_a = norm(a)
+        b = electrode_pos - end
+        # projection: H = a.cos(theta) = a.dot(b) / |a|
+        H = np.dot(b, a) / norm_a  # NB can be negative
+        L = H + norm_a
+        R2 = np.dot(b, b) - H ** 2  # NB squares
+        # To avoid numerical errors when electrode is placed (anywhere) on the
+        # section axis, enforce minimal axial distance
+        R2 = max(R2, (section.diam / 2.0 + 0.1) ** 2)
 
-        # total longitudinal distance from start of compartment
-        L = H + dis
+        if L < 0 and H < 0:  # electrode is "behind" section
+            num = np.sqrt(H ** 2 + R2) - H  # == norm(b) - H
+            denom = np.sqrt(L ** 2 + R2) - L
+        elif L > 0 and H < 0:  # electrode is "on top of" section
+            num = (np.sqrt(H ** 2 + R2) - H) * (L + np.sqrt(L ** 2 + R2))
+            denom = R2
+        else:  # electrode is "ahead of" section
+            num = np.sqrt(L ** 2 + R2) + L
+            denom = np.sqrt(H ** 2 + R2) + H  # == norm(b) + H
 
-        # if a.dot(b) < 0, projection will fall on the
-        # compartment.
-        if H < 0:
-            H = -H
-
-        # distance ^ 2 of electrode to end
-        r_sq = np.linalg.norm(ele_pos - end) ** 2
-
-        # phi
-        num = np.sqrt(H ** 2 + r_sq) - H
-        denom = np.sqrt(H ** 2 + r_sq) - L
-        phi = 1. / dis * np.log(num / denom)
+        phi = np.log(num / denom) / norm_a
 
     # [dis]: um; [sigma]: S / m
     # [phi / sigma] = [1/dis] / [sigma] = 1 / [dis] x [sigma]
@@ -134,6 +138,13 @@ def _transfer_resistance(section, ele_pos, sigma, method):
 class _LFPElectrode:
     """Local field potential (LFP) electrode class.
 
+    The handler is set up to maintain a vector of membrane currents at at every
+    inner segment of every section of every cell on each CVODE integration
+    step. In addition, it records a time vector of sample times. This class
+    must be instantiated and attached to the network during the building
+    process. It is used in conjunction with the calculation of extracellular
+    potentials.
+
     Parameters
     ----------
     coord : tuple
@@ -141,18 +152,16 @@ class _LFPElectrode:
     sigma : float
         Extracellular conductivity, in S/m, of the assumed infinite,
         homogeneous volume conductor that the cell and electrode are in.
-    pc : instance of h.ParallelContext()
-        ParallelContext instance for running in parallel
-    cvode : instanse of h.CVode
-        Multi order variable time step integration method.
     method : str
-        'psa' (default), i.e., point source approximation or line source
-        approximation, i.e., 'lsa'
+        Approximation to use. 'psa' (default) assigns all transmembrane
+        currents to the center point (0.5) (point source approximation).
+        'lsa' treats the section as a line source, and a single multiplier is
+        calculated for each section (line source approximation).
+    cvode : instance of h.CVode
+        Multi order variable time step integration method.
 
     Attributes
     ----------
-    lfp_t : instance of h.Vector
-        The time points (in ms) at which the LFP is calculated.
     lfp_v : instance of h.Vector
         The LFP voltage (in uV).
 
@@ -162,59 +171,61 @@ class _LFPElectrode:
     measured values of sigma in rat cortex (note units there are mS/cm)
     """
 
-    def __init__(self, coord, sigma=0.3, pc=None, cvode=None, method='psa'):
+    def __init__(self, coord, sigma=0.3, method='psa', cvode=None):
 
-        secs_in_network = _get_all_sections()  # ordered list of h.Sections
+        # ordered list of h.Sections on this rank (if running in parallel)
+        secs_on_rank = _get_sections_on_this_rank()
         # np.array of number of segments for each section, ordered as above
-        segment_counts = np.array(_get_segment_counts(secs_in_network))
+        segment_counts = np.array(_get_segment_counts(secs_on_rank))
 
         # pointers assigned to _ref_i_membrane_ at each EACH internal segment
         self.imem_ptrvec = h.PtrVector(segment_counts.sum())
         # placeholder into which pointer values are read on each sim step
-        self.imem_vec = h.Vector(int(self.imem_ptrvec.size()))
-        # transfer resistances, same length as membrane current pointers
-        self.r_transfer = np.empty((int(self.imem_ptrvec.size()), ),
-                                   dtype=np.int)
-        self.pc = pc
-        if self.pc is None:
-            self.pc = h.ParallelContext()
+        imem_vec_len = int(self.imem_ptrvec.size())
+        self.imem_vec = h.Vector(imem_vec_len)
 
-        # Enables fast calculation of transmembrane current (nA) at each
-        # segment
-        self.bscallback = cvode.extra_scatter_gather(0, self.callback)
-
-        xfer_resistance = list()
-        count = 0
-        for sec, n_segs in zip(secs_in_network, segment_counts):
+        transfer_resistance = list()
+        ptr_count = 0
+        for sec, n_segs in zip(secs_on_rank, segment_counts):
             this_xfer_r = _transfer_resistance(sec, coord, sigma=sigma,
                                                method=method)
             # the n_segs of this section get assigned the same value (e.g. in
             # PSA, the distance is calculated for the section mid point only)
-            xfer_resistance.extend([this_xfer_r] * n_segs)
+            transfer_resistance.extend([this_xfer_r] * n_segs)
             for seg in sec:  # section end points (0, 1) not included
                 # set Nth pointer to the net membrane current at this segment
-                self.imem_ptrvec.pset(count, sec(seg.x)._ref_i_membrane_)
-                count += 1
-        # convert to numpy array for speedy calculation in callback
-        self.r_transfer = np.array(xfer_resistance)
-        assert count == int(self.imem_ptrvec.size())  # smoke test
+                self.imem_ptrvec.pset(ptr_count, sec(seg.x)._ref_i_membrane_)
+                ptr_count += 1
 
-        self.lfp_t = h.Vector()
+        if ptr_count != imem_vec_len:
+            raise RuntimeError(f'Expected {imem_vec_len} imem pointers, '
+                               'got {count}.')
+
+        # transfer resistances for each segment (keep in NEURON object)
+        self.r_transfer = h.Vector(transfer_resistance)
+
+        # contributions of all segments on this rank to total calculated
+        # potential at electrode (_PC.allreduce called in _simulate_dipole)
         self.lfp_v = h.Vector()
 
-    def callback(self):
+        # Attach a callback for calculating the potentials at each time step.
+        # Enables fast calculation of transmembrane current (nA) at each
+        # segment. Note that this will run on each rank, so it is safe to use
+        # the extra_scatter_gather-method, which docs say doesn't support
+        # "multiple threads".
+        cvode.extra_scatter_gather(0, self.calc_potential_callback)
+
+    def reset(self):
+        self.lfp_v = h.Vector()
+        self.imem_vec = h.Vector(int(self.imem_ptrvec.size()))
+
+    def calc_potential_callback(self):
+
+        # 'gather' the values of seg.i_membrane_ into self.imem_vec
         self.imem_ptrvec.gather(self.imem_vec)
 
-        # verify sum i_membrane_ == stimulus
-        # s = pc.allreduce(imem_vec.sum(), 1)
-        # if rank == 0: print pc.t(0), s
-
-        # sum up the weighted i_membrane_. Result in vx
-        # rx.mulv(imem_vec, vx)
-
-        potential = np.dot(np.array(self.imem_vec.to_python()),
-                           self.r_transfer)
+        # multiply elementwise, then sum (dot-product): V = SUM (R_i x I_i)
+        potential = self.r_transfer.dot(self.imem_vec)
 
         # append to Vector
-        self.lfp_t.append(self.pc.t(0))
         self.lfp_v.append(potential)
