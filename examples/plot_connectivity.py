0814fc35b648256aaeb6fd33bbee3975a85d8460
Nick Tolley <nic.m.tolley@gmail.com>
2021-05-14 21:46:41 -0400

    Update connectivity example

--- examples/plot_connectivity.py
+++ examples/plot_connectivity.py
@@ -8,6 +8,9 @@ This example demonstrates how to modify the network connectivity.
 
 # Author: Nick Tolley <nicholas_tolley@brown.edu>
 
+# sphinx_gallery_thumbnail_number = 4
+
+from hnn_core.network import Network
 import os.path as op
 
 ###############################################################################
@@ -30,64 +33,85 @@ params = read_params(params_fname)
 # explore how it changes with new connections. We first instantiate the
 # network. (Note: Setting ``add_drives_from_params=True`` loads a set of
 # predefined drives without the drives API shown previously).
-net = default_network(params, add_drives_from_params=True)
+net_erp = default_network(params, add_drives_from_params=True)
 
 ###############################################################################
 # Instantiating the network comes with a predefined set of connections that
 # reflect the canonical neocortical microcircuit. ``net.connectivity``
 # is a list of dictionaries which detail every cell-cell, and drive-cell
 # connection.
-print(len(net.connectivity))
-print(net.connectivity[0:2])
+print(len(net_erp.connectivity))
+print(net_erp.connectivity[0:2])
 
 ###############################################################################
 # Data recorded during simulations are stored under
 # :class:`~hnn_core.Cell_Response`. To test multiple network structures, we can
 # create a copy of the original network. The copied network is then simulated.
-net_erp = net.copy()
 dpl_erp = simulate_dipole(net_erp, n_trials=1)
 net_erp.cell_response.plot_spikes_raster()
 
 ###############################################################################
-# We can modify the connectivity list to test the effect of different
-# connectivity patterns. For example, we can remove 90% all layer 2 inhibitory
-# connections.
-# This can be achieved with ``net.connectivity[idx].drop``, a function that is
-# available for each element of ``net.connectivity``. We can also visualize
-# any connection using ``net.connectivity.plot``.
-# Note that in the default network, the src_gids of each connection are
-# all the same cell type allowing for easy modifications.
-for conn in net.connectivity:
-    if conn['src_type'] == 'L2_basket':
-        conn.drop(0.1)
-
-net_remove = net.copy()
-dpl_remove = simulate_dipole(net_remove, n_trials=1)
-net_remove.cell_response.plot_spikes_raster()
-net.connectivity[10].plot()
-
-###############################################################################
-# That's a lot of spiking! Since basket cells are inhibitory, removing these
-# connections increases network wide excitability. We can additionally add
-# new connections using ``net.add_connection()``. Let's try connecting a
-# single layer 2 basket cell, to every layer 2 pyramidal cell. We can utilize
-# ``net.gid_ranges`` to help find the gids of interest.
+# We can also define our own connections to test the effect of different
+# connectivity patterns. To start, an empty network is instantiated.
+# ``add_drives_from_params`` can still be used with an empty Network, but
+# all cells will start disconnected. ``net.add_connection`` is then
+# used to create a custom network. Let us first create an all-to-all
+# connectivity pattern between the L5 pyramidal cells, and L2 basket cells.
 # :meth:`hnn_core.Network.add_connection` allows connections to be specified
 # with either cell names, or the gids directly. If multiple gids are provided
 # for either the sources or the targets, they will be connected in an
 # all-to-all pattern.
-print(net.gid_ranges)
-src_gid = net.gid_ranges['L2_basket'][0]
-target_gids = 'L2_pyramidal'
+
+net_all = Network(params, add_drives_from_params=True)
+
+# Pyramidal cell connections
+location, receptor = 'distal', 'ampa'
+weight, delay, lamtha = 1.0, 1.0, 70
+src = 'L5_pyramidal'
+for target in ['L5_pyramidal', 'L5_pyramidal']:
+    net_all.add_connection(src, target, location, receptor,
+                           delay, weight, lamtha)
+
+# Basket cell connections
 location, receptor = 'soma', 'gabaa'
 weight, delay, lamtha = 1.0, 1.0, 70
-net.add_connection(src_gid, target_gids, location, receptor,
-                   delay, weight, lamtha)
+src = 'L2_basket'
+for target in ['L2_basket', 'L5_pyramidal']:
+    net_all.add_connection(src, target, location, receptor,
+                           delay, weight, lamtha)
 
-net_add = net.copy()
-dpl_add = simulate_dipole(net_add, n_trials=1)
-net_add.cell_response.plot_spikes_raster()
-net.connectivity[-1].plot()
+dpl_all = simulate_dipole(net_all, n_trials=1)
+net_all.cell_response.plot_spikes_raster()
+
+###############################################################################
+# That's a lot of spiking! We can additionally use the ``probability``.
+# argument to create a sparse connectivity pattern instead of all-to-all. Let's
+# try creating the same network with a 10% change of cells connecting
+# to each other. The resulting connectivity pattern can also be visualized
+# with ``net.connectivity[idx].plot()``
+probability = 0.1
+net_sparse = Network(params, add_drives_from_params=True)
+
+# Pyramidal cell connections
+location, receptor = 'distal', 'ampa'
+weight, delay, lamtha = 1.0, 1.0, 70
+src = 'L5_pyramidal'
+for target in ['L5_pyramidal', 'L2_basket']:
+    net_sparse.add_connection(src, target, location, receptor,
+                              delay, weight, lamtha, probability)
+
+# Basket cell connections
+location, receptor = 'soma', 'gabaa'
+weight, delay, lamtha = 1.0, 1.0, 70
+src = 'L2_basket'
+for target in ['L2_basket', 'L5_pyramidal']:
+    net_sparse.add_connection(src, target, location, receptor,
+                              delay, weight, lamtha, probability)
+
+dpl_sparse = simulate_dipole(net_sparse, n_trials=1)
+net_sparse.cell_response.plot_spikes_raster()
+net_sparse.connectivity[-2].plot()
+net_sparse.connectivity[-1].plot()
 
 ###############################################################################
 # Adding a single inhibitory connection didn't completely restored the normal
@@ -98,8 +122,8 @@ import matplotlib.pyplot as plt
 from hnn_core.viz import plot_dipole
 fig, axes = plt.subplots(2, 1, sharex=True, figsize=(6, 6),
                          constrained_layout=True)
-dpls = [dpl_erp[0], dpl_remove[0], dpl_add[0]]
+dpls = [dpl_erp[0], dpl_all[0], dpl_sparse[0]]
 plot_dipole(dpls, ax=axes[0], layer='agg', show=False)
-axes[0].legend(['Normal', 'No L2 Basket', 'Single L2 Basket'])
+axes[0].legend(['Default', 'Custom All', 'Custom Sparse'])
 net_erp.cell_response.plot_spikes_hist(
     ax=axes[1], spike_types=['evprox', 'evdist'])
