0986a1a164680595d28e43cbc3f5011145ae0878
Nick Tolley <nic.m.tolley@gmail.com>
2021-06-06 14:31:25 -0400

    WIP: Start updating biophysics and connectivity according to Law et al.

--- hnn_core/__init__.py
+++ hnn_core/__init__.py
@@ -1,7 +1,8 @@
 from .dipole import simulate_dipole, read_dipole, average_dipoles
 from .drives import drive_event_times
 from .params import Params, read_params
-from .network import Network, default_network
+from .network import Network
+from .network_models import default_network, beta_erp_network
 from .cell import Cell
 from .cell_response import CellResponse, read_spikes
 from .cells_default import pyramidal, basket
