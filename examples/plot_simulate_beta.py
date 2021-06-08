b085b44407fb183c4c49d46b615a24e26fccd852
Nick Tolley <nic.m.tolley@gmail.com>
2021-06-07 22:28:38 -0400

    Add beta plot to example

--- examples/plot_simulate_beta.py
+++ examples/plot_simulate_beta.py
@@ -9,38 +9,28 @@ by prestimulus beta events.
 
 # Authors: Nick Tolley <nicholas_tolley@brown.edu>
 
-import os.path as op
-
 ###############################################################################
 # Let us import hnn_core
-
 import hnn_core
-from hnn_core import simulate_dipole, read_params, beta_erp_network
-
-###############################################################################
-# Then we setup the directories and read the default parameters file
-hnn_core_root = op.dirname(hnn_core.__file__)
-params_fname = op.join(hnn_core_root, 'param', 'default.json')
-params = read_params(params_fname)
-print(params)
+from hnn_core import simulate_dipole, beta_erp_network
+from hnn_core.viz import plot_dipole
 
 ###############################################################################
 # We begin by instantiating the network model described in Law et al. 2021.
-net = beta_erp_network(params)
+net = beta_erp_network()
 
 ###############################################################################
 # Next a beta event is created by inducing simultaneous proximal
 # distal drives.
-
 weights_ampa_p1 = {'L2_basket': 0.00004, 'L2_pyramidal': 0.00002,
                    'L5_basket': 0.00002, 'L5_pyramidal': 0.00002}
 syn_delays_p1 = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
-                      'L5_basket': 1.0, 'L5_pyramidal': 1.0}
+                 'L5_basket': 1.0, 'L5_pyramidal': 1.0}
 # What spike ISI??
 net.add_bursty_drive(
-    'beta_prox', tstart=40., burst_rate=10, burst_std=20., numspikes=2,
-    spike_isi=1, repeats=10, location='proximal', weights_ampa=weights_ampa_p1,
-    synaptic_delays=syn_delays_p1, seedcore=14)
+    'beta_prox', tstart=40., burst_rate=1e-10, burst_std=20., numspikes=2,
+    spike_isi=10, repeats=10, location='proximal',
+    weights_ampa=weights_ampa_p1, synaptic_delays=syn_delays_p1, seedcore=14)
 
 # Distal Drive
 weights_ampa_d1 = {'L2_basket': 0.00032, 'L2_pyramidal': 0.00008,
@@ -49,6 +39,17 @@ syn_delays_d1 = {'L2_basket': 0.5, 'L2_pyramidal': 0.5,
                       'L5_pyramidal': 0.5}
 
 net.add_bursty_drive(
-    'beta_dist', tstart=40., burst_rate=10, burst_std=10., numspikes=2,
-    spike_isi=1, repeats=10, location='distal', weights_ampa=weights_ampa_d1,
+    'beta_dist', tstart=40., burst_rate=1e-10, burst_std=10., numspikes=2,
+    spike_isi=10, repeats=10, location='distal', weights_ampa=weights_ampa_d1,
     synaptic_delays=syn_delays_d1, seedcore=14)
+
+###############################################################################
+# And finally we simulate
+import matplotlib.pyplot as plt
+dpls = simulate_dipole(net)
+net.cell_response.plot_spikes_hist()
+
+fig, axes = plt.subplots(2, 1, sharex=True, figsize=(6, 6),
+                         constrained_layout=True)
+plot_dipole(dpls, ax=axes[0], layer='agg', show=False)
+net.cell_response.plot_spikes_hist(ax=axes[1])
