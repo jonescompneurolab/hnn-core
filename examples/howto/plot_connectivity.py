9f1b58acef6285bbac8956000b0104f9ac075e32
Nick Tolley <nic.m.tolley@gmail.com>
2021-07-11 01:29:39 -0400

    Less weird example?

--- examples/howto/plot_connectivity.py
+++ examples/howto/plot_connectivity.py
@@ -120,10 +120,10 @@ net_sparse.cell_response.plot_spikes_raster()
 
 ###############################################################################
 # We can plot the sparse connectivity pattern between cell populations.
-conn_idx = len(net_sparse.connectivity) - 1
-plot_connectivity_matrix(net_sparse, conn_idx)
-
-conn_idx = len(net_sparse.connectivity) - 2
+src_gids, target_gids = 'L2_basket', 'L2_basket'
+loc, receptor = 'soma', 'gabaa'
+conn_indices = net_erp.pick_connection(src_gids, target_gids, loc, receptor)
+conn_idx = conn_indices[0]
 plot_connectivity_matrix(net_sparse, conn_idx)
 
 ###############################################################################
