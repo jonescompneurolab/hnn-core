587f8dbd2d8ff200bd44b074fd5c4256d96e2eb6
Nick Tolley <nic.m.tolley@gmail.com>
2021-08-20 10:21:04 -0400

    Update examples

--- examples/workflows/plot_simulate_alpha.py
+++ examples/workflows/plot_simulate_alpha.py
@@ -45,8 +45,8 @@ syn_delays_p = {'L2_pyramidal': 0.1, 'L5_pyramidal': 1.}
 
 net.add_bursty_drive(
     'alpha_prox', tstart=50., burst_rate=10, burst_std=burst_std, numspikes=2,
-    spike_isi=10, n_drive_cells=10, location=location, weights_ampa=weights_ampa_p,
-    synaptic_delays=syn_delays_p, seedcore=14)
+    spike_isi=10, n_drive_cells=10, location=location,
+    weights_ampa=weights_ampa_p, synaptic_delays=syn_delays_p, event_seed=14)
 
 # simulate the dipole, but do not automatically scale or smooth the result
 dpl = simulate_dipole(net, tstop=310., n_trials=1)
@@ -94,8 +94,8 @@ weights_ampa_d = {'L2_pyramidal': 5.4e-5, 'L5_pyramidal': 5.4e-5}
 syn_delays_d = {'L2_pyramidal': 5., 'L5_pyramidal': 5.}
 net.add_bursty_drive(
     'alpha_dist', tstart=50., burst_rate=10, burst_std=burst_std, numspikes=2,
-    spike_isi=10, n_drive_cells=10, location=location, weights_ampa=weights_ampa_d,
-    synaptic_delays=syn_delays_d, seedcore=16)
+    spike_isi=10, n_drive_cells=10, location=location,
+    weights_ampa=weights_ampa_d, synaptic_delays=syn_delays_d, event_seed=16)
 
 dpl = simulate_dipole(net, tstop=310., n_trials=1)
 
