cdd568e807bce20c8eefd6c5de7c493f0fc4ac6e
Mainak Jas <mainakjas@gmail.com>
2021-09-17 17:24:28 -0400

    FIX test

--- examples/workflows/plot_simulate_evoked.py
+++ examples/workflows/plot_simulate_evoked.py
@@ -52,6 +52,7 @@ print(params['L2Pyr_soma*'])
 # inside it.
 net = jones_2009_model(params)
 net.plot_cells()
+sdfdfd
 net.cell_types['L5_pyramidal'].plot_morphology()
 
 ###############################################################################
