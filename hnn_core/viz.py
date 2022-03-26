0f912c2abf9859110d736e64df2456b8e382912a
Huzi Cheng <tigeraus@icloud.com>
2022-03-26 04:57:10 -0400

    Update hnn_core/viz.py
    Co-authored-by: Mainak Jas <jasmainak@users.noreply.github.com>

--- hnn_core/viz.py
+++ hnn_core/viz.py
@@ -252,7 +252,6 @@ def plot_dipole(dpl, tmin=None, tmax=None, ax=None, layer='agg', decim=None,
     if isinstance(dpl, Dipole):
         dpl = [dpl]
     else:
-        # add average plot when we have dpl>1 and `average` is specified.
         if average:
             dpl = dpl + [average_dipoles(dpl)]
 
