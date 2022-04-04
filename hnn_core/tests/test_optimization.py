4f9763bbb6abb78b6ad5cdf4d1de1a5e0f945f4a
mohdsherif <molpsychistb@gmail.com>
2022-04-04 14:18:27 -0400

    added a function to test optimizing selected drives

--- hnn_core/tests/test_optimization.py
+++ hnn_core/tests/test_optimization.py
@@ -136,3 +136,81 @@ def test_optimize_evoked():
 
     # static drive params should remain constant
     assert drive_static_params_opt == drive_static_params_orig
+
+def test_optimize_evoked_selectedDrive():
+    """Test running optimzing a selected drive while parameters of other drives stay
+    the same in a reduced network."""
+    hnn_core_root = op.dirname(hnn_core.__file__)
+    params_fname = op.join(hnn_core_root, 'param', 'default.json')
+    params = read_params(params_fname)
+
+    tstop = 10.
+    n_trials = 1
+
+    # simulate a dipole to establish ground-truth drive parameters
+    mu_orig = 6.
+    params.update({'N_pyr_x': 3,
+                   'N_pyr_y': 3,
+                   't_evprox_1': mu_orig,
+                   'sigma_t_evprox_1': 2.,
+                   't_evdist_1': mu_orig + 2,
+                   'sigma_t_evdist_1': 2.})
+    net_orig = jones_2009_model(params, add_drives_from_params=True)
+    del net_orig.external_drives['evprox2'] # will keep evprox1 and evdist1
+    dpl_orig = simulate_dipole(net_orig, tstop=tstop, n_trials=n_trials)[0]
+
+    # simulate a dipole with a time-shifted evprox1 and evdist1 drives
+    mu_offset = 4.
+    params.update({'N_pyr_x': 3,
+                   'N_pyr_y': 3,
+                   't_evprox_1': mu_offset,
+                   'sigma_t_evprox_1': 2.,
+                   't_evdist_1': mu_offset + 2,
+                   'sigma_t_evdist_1': 2.})
+    net_offset = jones_2009_model(params, add_drives_from_params=True)
+    del net_offset.external_drives['evprox2']
+    dpl_offset = simulate_dipole(net_offset, tstop=tstop, n_trials=n_trials)[0]
+    # get drive params from the pre-optimization Network instance
+    _, _, drive_static_params_orig = _get_drive_params(net_offset, ['evprox1'])
+
+    with pytest.raises(ValueError, match='The current Network instance lacks '
+                       'any evoked drives'):
+        net_empty = net_offset.copy()
+        del net_empty.external_drives['evprox1']
+        del net_empty.external_drives['evdist1']
+        net_opt = optimize_evoked(net_empty, tstop=tstop, n_trials=n_trials,
+                                  target_dpl=dpl_orig, initial_dpl=dpl_offset)
+
+    # run optimization only on evprox1
+    which_drives = ['evprox1']
+    net_opt = optimize_evoked(net_offset, tstop=tstop, n_trials=n_trials,
+                              target_dpl=dpl_orig, initial_dpl=dpl_offset,
+                              timing_range_multiplier=3.,
+                              sigma_range_multiplier=50.,
+                              synweight_range_multiplier=500.,
+                              maxiter=10, which_drives=which_drives)
+
+    # the names of drives should be preserved during optimization
+    assert net_offset.external_drives.keys() == net_opt.external_drives.keys()
+
+    drive_dynamics_opt, drive_syn_weights_opt, drive_static_params_opt = \
+        _get_drive_params(net_opt, ['evprox1'])
+
+    # ensure that params corresponding to only one evoked drive are discovered
+    assert (len(drive_dynamics_opt) ==
+            len(drive_syn_weights_opt) ==
+            len(drive_static_params_opt) == 1)
+
+    # static drive params should remain constant
+    assert drive_static_params_opt == drive_static_params_orig
+
+
+    # ensure that only the drive that we wanted to optimzie over made that change, while the others were constant.  CONTINUE HERERERERERERE
+    drive_evdist1_dynamics_offset, drive_evdist1_syn_weights_offset, drive_static_params_offset = \
+        _get_drive_params(net_offset, ['evdist1'])
+    drive_evdist1_dynamics_opt, drive_evdist1_syn_weights_opt, drive_static_params_opt = \
+        _get_drive_params(net_opt, ['evdist1'])
+
+    # assert that evdist1 did NOT change
+    assert drive_evdist1_dynamics_opt == drive_evdist1_dynamics_offset
+    assert drive_evdist1_syn_weights_opt == drive_evdist1_syn_weights_offset
