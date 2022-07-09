01e47aa2d71bf3c1c9ad7ef5347703bf9a14cee0
Mainak Jas <mainakjas@gmail.com>
2022-07-09 14:09:33 -0400

    MAINT: no env variables

--- hnn_core/tests/test_gui.py
+++ hnn_core/tests/test_gui.py
@@ -6,6 +6,5 @@ from hnn_core.gui.gui import run_hnn_gui
 
 def test_run_gui():
     """Test if main gui function gives proper ipywidget"""
-    os.environ["DEBUG_HNNGUI"] = "0"
     gui = run_hnn_gui()
     assert gui is not None
