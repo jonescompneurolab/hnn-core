a4ec8a244e0420d9027bf36c85948f8a5d6fddae
chenghuzi <tigeraus@icloud.com>
2022-07-13 11:41:45 -0400

    Fix gui test

--- hnn_core/tests/test_gui.py
+++ hnn_core/tests/test_gui.py
@@ -1,8 +1,9 @@
 # Authors: Huzi Cheng <hzcheng15@icloud.com>
-from hnn_core.gui.gui import run_hnn_gui
+from hnn_core.gui.gui import HNNGUI
 
 
 def test_run_gui():
     """Test if main gui function gives proper ipywidget"""
-    gui = run_hnn_gui()
-    assert gui is not None
+    gui = HNNGUI()
+    app_layout = gui.run()
+    assert app_layout is not None
