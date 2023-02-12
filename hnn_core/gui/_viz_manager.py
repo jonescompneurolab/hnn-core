1c77cf37ab8e3dfbb50c9fc983583821164a286e
chenghuzi <tigeraus@icloud.com>
2023-02-11 23:23:39 -0500

    Fix axis label missing bug

--- hnn_core/gui/_viz_manager.py
+++ hnn_core/gui/_viz_manager.py
@@ -97,13 +97,20 @@ def _update_ax(fig, ax, single_simulation, sim_name, plot_type, plot_config):
     net_copied = copy.deepcopy(single_simulation['net'])
 
     for dpl in dpls_copied:
-        dpl.smooth(plot_config['dipole_smooth']).scale(
-            plot_config['dipole_scaling'])
+        if plot_config['dipole_smooth'] > 0:
+            dpl.smooth(plot_config['dipole_smooth']).scale(
+                plot_config['dipole_scaling'])
+        else:
+            dpl.scale(plot_config['dipole_scaling'])
 
     if net_copied is None:
         print("No network data")
         return
 
+    # Explicitly do this in case the
+    # x and y axis are hidden after plotting some functions.
+    ax.get_yaxis().set_visible(True)
+    ax.get_xaxis().set_visible(True)
     if plot_type == 'spikes':
         if net_copied.cell_response:
             net_copied.cell_response.plot_spikes_raster(ax=ax, show=False)
@@ -124,7 +131,7 @@ def _update_ax(fig, ax, single_simulation, sim_name, plot_type, plot_config):
             max_f = plot_config['max_spectral_frequency']
             step_f = 1.0
             freqs = np.arange(min_f, max_f, step_f)
-            n_cycles = freqs / 8.
+            n_cycles = 7  # freqs / 8.
             dpls_copied[0].plot_tfr_morlet(
                 freqs,
                 n_cycles=n_cycles,
