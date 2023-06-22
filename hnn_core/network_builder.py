f029f11d272a094edc446740b746a22e297e62ae
raj1701 <rajatpartani@gmail.com>
2023-06-22 11:42:56 +0000

    Documentation attempt 1 and removing load mechanisms from other place

--- hnn_core/network_builder.py
+++ hnn_core/network_builder.py
@@ -313,7 +313,8 @@ class NetworkBuilder(object):
         self._rank = _get_rank()
 
         # load mechanisms needs ParallelContext for get_rank
-        load_custom_mechanisms()
+        # Not required here as already loaded in building cells
+        # load_custom_mechanisms()
 
         if self._rank == 0:
             print('Building the NEURON model')
