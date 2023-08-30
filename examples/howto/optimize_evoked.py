16797ce2a628de9336af737ab0b997e6eb13a1b2
Carolina Fernandez <cxf418@miami.edu>
2023-08-30 15:29:52 -0400

    update example

--- examples/howto/optimize_evoked.py
+++ examples/howto/optimize_evoked.py
@@ -43,6 +43,66 @@ urlretrieve(data_url, 'S1_SupraT.txt')
 exp_dpl = read_dipole('S1_SupraT.txt')
 
 ###############################################################################
+# Let’s then simulate the dipole with some initial parameters.
+
+tstop = exp_dpl.times[-1]
+scale_factor = 3000
+smooth_window_len = 30
+
+net_init = jones_2009_model()
+
+# Proximal 1
+weights_ampa_p1 = {'L2_basket': 0.2913, 'L2_pyramidal': 0.9337,
+                   'L5_basket': 0.1951, 'L5_pyramidal': 0.3602}
+weights_nmda_p1 = {'L2_basket': 0.9240, 'L2_pyramidal': 0.0845,
+                   'L5_basket': 0.5849, 'L5_pyramidal': 0.65105}
+synaptic_delays_p = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                     'L5_basket': 1., 'L5_pyramidal': 1.}
+net_init.add_evoked_drive('evprox1',
+                          mu=5.6813,
+                          sigma=20.3969,
+                          numspikes=1,
+                          location='proximal',
+                          weights_ampa=weights_ampa_p1,
+                          weights_nmda=weights_nmda_p1,
+                          synaptic_delays=synaptic_delays_p)
+
+# Distal
+weights_ampa_d1 = {'L2_basket': 0.8037, 'L2_pyramidal': 0.5738,
+                   'L5_pyramidal': 0.3626}
+weights_nmda_d1 = {'L2_basket': 0.2492, 'L2_pyramidal': 0.6183,
+                   'L5_pyramidal': 0.1121}
+synaptic_delays_d1 = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                      'L5_pyramidal': 0.1}
+net_init.add_evoked_drive('evdist1',
+                          mu=58.6539,
+                          sigma=5.5810,
+                          numspikes=1,
+                          location='distal',
+                          weights_ampa=weights_ampa_d1,
+                          weights_nmda=weights_nmda_d1,
+                          synaptic_delays=synaptic_delays_d1)
+
+# Proximal 2
+weights_ampa_p2 = {'L2_basket': 0.01, 'L2_pyramidal': 0.01, 'L5_basket': 0.01,
+                   'L5_pyramidal': 0.01}
+weights_nmda_p2 = {'L2_basket': 0.01, 'L2_pyramidal': 0.01, 'L5_basket': 0.01,
+                   'L5_pyramidal': 0.01}
+net_init.add_evoked_drive('evprox2',
+                          mu=80,
+                          sigma=1,
+                          numspikes=1,
+                          location='proximal',
+                          weights_ampa=weights_ampa_p2,
+                          weights_nmda=weights_nmda_p2,
+                          synaptic_delays=synaptic_delays_p)
+
+with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
+    init_dpl = simulate_dipole(net_init, tstop=tstop, n_trials=1)[0]
+init_dpl.scale(scale_factor)
+init_dpl.smooth(smooth_window_len)
+
+###############################################################################
 # Now we start the optimization!
 #
 # First, we define a function that will tell the optimization routine how to
@@ -54,36 +114,31 @@ exp_dpl = read_dipole('S1_SupraT.txt')
 def set_params(net, params):
 
     # Proximal 1
-    weights_ampa_p1 = {'L2_basket':
-                       params['evprox1_ampa_L2_basket'],
-                       'L2_pyramidal':
-                       params['evprox1_ampa_L2_pyramidal'],
-                       'L5_basket':
-                       params['evprox1_ampa_L5_basket'],
-                       'L5_pyramidal':
-                       params['evprox1_ampa_L5_pyramidal']}
+    weights_ampa_p1 = {'L2_basket': 0.2913, 'L2_pyramidal': 0.9337,
+                       'L5_basket': 0.1951, 'L5_pyramidal': 0.3602}
+    weights_nmda_p1 = {'L2_basket': 0.9240, 'L2_pyramidal': 0.0845,
+                       'L5_basket': 0.5849, 'L5_pyramidal': 0.65105}
     synaptic_delays_p = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
                          'L5_basket': 1., 'L5_pyramidal': 1.}
-
     net.add_evoked_drive('evprox1',
-                         mu=params['evprox1_mu'],
-                         sigma=params['evprox1_sigma'],
+                         mu=5.6813,
+                         sigma=20.3969,
                          numspikes=1,
                          location='proximal',
                          weights_ampa=weights_ampa_p1,
+                         weights_nmda=weights_nmda_p1,
                          synaptic_delays=synaptic_delays_p)
 
     # Distal
-    weights_ampa_d1 = {'L2_basket': 0.006562, 'L2_pyramidal': 7e-06,
-                       'L5_pyramidal': 0.1423}
-    weights_nmda_d1 = {'L2_basket': 0.019482, 'L2_pyramidal': 0.004317,
-                       'L5_pyramidal': 0.080074}
+    weights_ampa_d1 = {'L2_basket': 0.8037, 'L2_pyramidal': 0.5738,
+                       'L5_pyramidal': 0.3626}
+    weights_nmda_d1 = {'L2_basket': 0.2492, 'L2_pyramidal': 0.6183,
+                       'L5_pyramidal': 0.1121}
     synaptic_delays_d1 = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
                           'L5_pyramidal': 0.1}
-
     net.add_evoked_drive('evdist1',
-                         mu=63.53,
-                         sigma=3.85,
+                         mu=58.6539,
+                         sigma=5.5810,
                          numspikes=1,
                          location='distal',
                          weights_ampa=weights_ampa_d1,
@@ -91,15 +146,29 @@ def set_params(net, params):
                          synaptic_delays=synaptic_delays_d1)
 
     # Proximal 2
-    weights_ampa_p2 = {'L2_basket': 3e-06, 'L2_pyramidal': 1.43884,
-                       'L5_basket': 0.008958, 'L5_pyramidal': 0.684013}
-
+    weights_ampa_p2 = {'L2_basket':
+                       params['evprox2_ampa_L2_basket'],
+                       'L2_pyramidal':
+                       params['evprox2_ampa_L2_pyramidal'],
+                       'L5_basket':
+                       params['evprox2_ampa_L5_basket'],
+                       'L5_pyramidal':
+                       params['evprox2_ampa_L5_pyramidal']}
+    weights_nmda_p2 = {'L2_basket':
+                       params['evprox2_nmda_L2_basket'],
+                       'L2_pyramidal':
+                       params['evprox2_nmda_L2_pyramidal'],
+                       'L5_basket':
+                       params['evprox2_nmda_L5_basket'],
+                       'L5_pyramidal':
+                       params['evprox2_nmda_L5_pyramidal']}
     net.add_evoked_drive('evprox2',
-                         mu=137.12,
-                         sigma=8.33,
+                         mu=params['evprox2_mu'],
+                         sigma=params['evprox2_sigma'],
                          numspikes=1,
                          location='proximal',
                          weights_ampa=weights_ampa_p2,
+                         weights_nmda=weights_nmda_p2,
                          synaptic_delays=synaptic_delays_p)
 
 ###############################################################################
@@ -112,27 +181,26 @@ def set_params(net, params):
 # The following synaptic weight parameter ranges (units of micro-siemens)
 # were chosen so as to keep the model in physiologically realistic regimes.
 
-
-constraints = dict({'evprox1_ampa_L2_basket': (0.01, 1.),
-                    'evprox1_ampa_L2_pyramidal': (0.01, 1.),
-                    'evprox1_ampa_L5_basket': (0.01, 1.),
-                    'evprox1_ampa_L5_pyramidal': (0.01, 1.),
-                    'evprox1_mu': (5., 50.),
-                    'evprox1_sigma': (2., 25.)})
+constraints = dict({'evprox2_ampa_L2_basket': (0.01, 1.),
+                    'evprox2_ampa_L2_pyramidal': (0.01, 1.),
+                    'evprox2_ampa_L5_basket': (0.01, 1.),
+                    'evprox2_ampa_L5_pyramidal': (0.01, 1.),
+                    'evprox2_nmda_L2_basket': (0.01, 1.),
+                    'evprox2_nmda_L2_pyramidal': (0.01, 1.),
+                    'evprox2_nmda_L5_basket': (0.01, 1.),
+                    'evprox2_nmda_L5_pyramidal': (0.01, 1.),
+                    'evprox2_mu': (100., 120.),
+                    'evprox2_sigma': (2., 30.)})
 
 ###############################################################################
 # Now we define and fit the optimizer.
 
 from hnn_core.optimization import Optimizer
 
-tstop = exp_dpl.times[-1]
-scale_factor = 3000
-smooth_window_len = 30
-
 net = jones_2009_model()
 optim = Optimizer(net, tstop=tstop, constraints=constraints,
                   set_params=set_params, scale_factor=scale_factor,
-                  smooth_window_len=smooth_window_len, solver='cobyla')
+                  smooth_window_len=smooth_window_len, solver='bayesian')
 with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
     optim.fit(exp_dpl.data['agg'])
 
@@ -147,9 +215,11 @@ opt_dpl.smooth(smooth_window_len)
 
 fig, axes = plt.subplots(2, 1, sharex=True, figsize=(6, 6))
 
+# plot original
 exp_dpl.plot(ax=axes[0], layer='agg', show=False, color='tab:blue')
+init_dpl.plot(ax=axes[0], layer='agg', show=False, color='tab:orange')
 opt_dpl.plot(ax=axes[0], layer='agg', show=False, color='tab:green')
-axes[0].legend(['experimental', 'optimized'])
+axes[0].legend(['experimental', 'initial', 'optimized'])
 optim.net_.cell_response.plot_spikes_hist(ax=axes[1])
 
 fig1 = optim.plot_convergence()
