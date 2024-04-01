a441fde744447fee20e2a03bd1d2bed1bdedb944
Carolina Fernandez <cxf418@miami.edu>
2024-04-01 17:08:40 -0400

    allow users to set template-objective-function-specific obj_fun_kwargs

--- hnn_core/tests/test_general_optimization.py
+++ hnn_core/tests/test_general_optimization.py
@@ -86,7 +86,7 @@ def test_optimize_evoked(solver):
     # test repr before fitting
     assert 'fit=False' in repr(optim), "optimizer is already fit"
 
-    optim.fit(dpl_orig)
+    optim.fit(target=dpl_orig)
 
     # test repr after fitting
     assert 'fit=True' in repr(optim), "optimizer was not fit"
