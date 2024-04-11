e94c935e8afe8587afd60c7b843b2e176a27f0f0
George Dang <53052793+gtdang@users.noreply.github.com>
2024-04-11 14:44:14 -0400

    refactor: moved base_network fixture to test_network.py

--- hnn_core/tests/test_network.py
+++ hnn_core/tests/test_network.py
@@ -17,6 +17,46 @@ hnn_core_root = op.dirname(hnn_core.__file__)
 params_fname = op.join(hnn_core_root, 'param', 'default.json')
 
 
+@pytest.fixture(scope="class")
+def base_network():
+    """ Base Network with connections and drives """
+    params_fname = op.join(hnn_core_root, 'param', 'default.json')
+    params = read_params(params_fname)
+    net = Network(params, legacy_mode=False)
+    # add some basic local network connectivity
+    # layer2 Pyr -> layer2 Pyr
+    # layer5 Pyr -> layer5 Pyr
+    for target_cell in ['L2_pyramidal', 'L5_pyramidal']:
+        for receptor in ['nmda', 'ampa']:
+            net.add_connection(
+                target_cell, target_cell, loc='proximal', receptor=receptor,
+                weight=5e-4, delay=net.delay, lamtha=3.0, allow_autapses=False)
+    # layer2 Basket -> layer2 Pyr
+    # layer5 Basket -> layer5 Pyr
+    for receptor in ['gabaa', 'gabab']:
+        net.add_connection(
+            src_gids='L2_basket', target_gids='L2_pyramidal', loc='soma',
+            receptor=receptor, weight=5e-4, delay=net.delay, lamtha=50.0)
+        net.add_connection(
+            src_gids='L5_basket', target_gids='L2_pyramidal', loc='soma',
+            receptor=receptor, weight=5e-4, delay=net.delay, lamtha=70.0)
+    # layer2 Basket -> layer2 Basket (autapses allowed)
+    net.add_connection(
+        src_gids='L2_basket', target_gids='L2_basket', loc='soma',
+        receptor='gabaa', weight=5e-4, delay=net.delay, lamtha=20.0)
+
+    # add arbitrary drives that contribute artificial cells to network
+    net.add_evoked_drive(name='evdist1', mu=5.0, sigma=1.0,
+                         numspikes=1, location='distal',
+                         weights_ampa={'L2_basket': 0.1,
+                                       'L2_pyramidal': 0.1})
+    net.add_evoked_drive(name='evprox1', mu=5.0, sigma=1.0,
+                         numspikes=1, location='proximal',
+                         weights_ampa={'L2_basket': 0.1,
+                                       'L2_pyramidal': 0.1})
+    return net, params
+
+
 def test_network_models():
     """"Test instantiations of the network object"""
     # Make sure critical biophysics for Law model are updated
@@ -530,42 +570,8 @@ def test_network_drives_legacy():
                                                  n_bursty_sources)
 
 
-def test_network_connectivity():
-    """Test manipulation of local network connectivity."""
-    params = read_params(params_fname)
-    net = Network(params, legacy_mode=False)
-
-    # add some basic local network connectivity
-    # layer2 Pyr -> layer2 Pyr
-    # layer5 Pyr -> layer5 Pyr
-    for target_cell in ['L2_pyramidal', 'L5_pyramidal']:
-        for receptor in ['nmda', 'ampa']:
-            net.add_connection(
-                target_cell, target_cell, loc='proximal', receptor=receptor,
-                weight=5e-4, delay=net.delay, lamtha=3.0, allow_autapses=False)
-    # layer2 Basket -> layer2 Pyr
-    # layer5 Basket -> layer5 Pyr
-    for receptor in ['gabaa', 'gabab']:
-        net.add_connection(
-            src_gids='L2_basket', target_gids='L2_pyramidal', loc='soma',
-            receptor=receptor, weight=5e-4, delay=net.delay, lamtha=50.0)
-        net.add_connection(
-            src_gids='L5_basket', target_gids='L2_pyramidal', loc='soma',
-            receptor=receptor, weight=5e-4, delay=net.delay, lamtha=70.0)
-    # layer2 Basket -> layer2 Basket (autapses allowed)
-    net.add_connection(
-        src_gids='L2_basket', target_gids='L2_basket', loc='soma',
-        receptor='gabaa', weight=5e-4, delay=net.delay, lamtha=20.0)
-
-    # add arbitrary drives that contribute artificial cells to network
-    net.add_evoked_drive(name='evdist1', mu=5.0, sigma=1.0,
-                         numspikes=1, location='distal',
-                         weights_ampa={'L2_basket': 0.1,
-                                       'L2_pyramidal': 0.1})
-    net.add_evoked_drive(name='evprox1', mu=5.0, sigma=1.0,
-                         numspikes=1, location='proximal',
-                         weights_ampa={'L2_basket': 0.1,
-                                       'L2_pyramidal': 0.1})
+def test_network_connectivity(base_network):
+    net, params = base_network
 
     # instantiate drive events and artificial cells for NetworkBuilder
     net._instantiate_drives(tstop=10.0,
