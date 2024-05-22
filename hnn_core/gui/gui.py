649c90ae0c008ad9f67024e221eec2a3e4167a76
Camilo Diaz <k.diaz99@gmail.com>
2024-05-22 10:09:47 -0400

    MAINT: Removing debug code

--- hnn_core/gui/gui.py
+++ hnn_core/gui/gui.py
@@ -1590,9 +1590,5 @@ def launch():
     You can pass voila commandline parameters as usual.
     """
     from voila.app import main
-    import debugpy
-    debugpy.listen(5678)
-    print("Waiting for debugger attach")
-    debugpy.wait_for_client()
     notebook_path = Path(__file__).parent / 'hnn_widget.ipynb'
     main([str(notebook_path.resolve()), *sys.argv[1:]])
