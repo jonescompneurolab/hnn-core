3cd90de8f742b11bf7aec039ec1998db8cefe963
Camilo Diaz <k.diaz99@gmail.com>
2024-06-09 11:31:19 -0400

    add neuron-mpi enabled test

--- hnn_core/mpi_test.py
+++ hnn_core/mpi_test.py
@@ -1,6 +1,6 @@
 from mpi4py import MPI
 import logging
-
+from neuron import h
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
@@ -9,3 +9,17 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 logger.info(f"Process {rank} out of {size} processors")
+# Test NEURON MPI support
+try:
+    h('''
+    if (nrnmpi_use) {
+        printf("NEURON MPI is enabled. Running on %d processes\\n", nrnmpi_numprocs_world)
+    } else {
+        printf("NEURON MPI is not enabled.\\n")
+    }
+    ''')
+except Exception as e:
+    logger.error(f"Error testing NEURON MPI support: {e}")
+
+if rank == 0:
+    print("NEURON MPI test completed")
