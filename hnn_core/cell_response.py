14032967578a00dd1c74b9ff977028e899415737
katduecker <katharina.duecker@gmail.com>
2024-06-28 15:37:57 -0400

    rebase

--- hnn_core/cell_response.py
+++ hnn_core/cell_response.py
@@ -233,6 +233,10 @@ class CellResponse(object):
     @property
     def isec(self):
         return self._isec
+    
+    @property
+    def ca(self):
+        return self._ca
 
     @property
     def ca(self):
