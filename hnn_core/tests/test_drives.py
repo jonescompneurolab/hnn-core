1250c73b975a2e35d26a2ecc1db7ea06d9341d20
dylansdaniels-berkeley <dylansdaniels@berkeley.edu>
2024-07-10 11:31:31 -0400

    paramtrize new test

--- hnn_core/tests/test_drives.py
+++ hnn_core/tests/test_drives.py
@@ -467,18 +467,23 @@ def test_drive_random_state():
             net.external_drives['evprox2']['events'])
 
 
-def test_non_cellspecific_pois_rate(setup_net):
+@pytest.mark.parametrize("rate_constant,cell_specific,n_drive_cells",
+                         [(2, False, 1), (2.0, False, 1),
+                          (2, True, 'n_cells'), (2.0, True, 'n_cells'),
+                          ])
+def test_non_cellspecific_pois_rate(setup_net, rate_constant,
+                                    cell_specific, n_drive_cells):
     """Testing rate constant when adding non-cell-specific poisson drive"""
 
     net = setup_net
-    rate_constant = 2.0
 
     weights_ampa_noise = {'L2_basket': 0.01, 'L2_pyramidal': 0.002,
                         'L5_pyramidal': 0.02}
     
     net.add_poisson_drive('noise_global', rate_constant=rate_constant,
-                        location='distal', weights_ampa=weights_ampa_noise,space_constant=100, n_drive_cells=1,
-                        cell_specific=False)
+                        location='distal', weights_ampa=weights_ampa_noise,
+                        space_constant=100, n_drive_cells=n_drive_cells,
+                        cell_specific=cell_specific)
 
-    assert rate_constant == net.external_drives['noise_global']['dynamics']['rate_constant']
+    simulate_dipole(net, tstop=5)
     
