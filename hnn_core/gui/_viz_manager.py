ecea54fa94ec0ae6b4b2be4bfa0feea02c437c85
George Dang <53052793+gtdang@users.noreply.github.com>
2024-10-24 11:29:24 -0400

    refactor: change default smoothing for dipoles to 0

--- hnn_core/gui/_viz_manager.py
+++ hnn_core/gui/_viz_manager.py
@@ -565,7 +565,7 @@ def _get_ax_control(widgets, data, fig_idx, fig, ax):
         style=analysis_style,
     )
     simulation_dipole_smooth = FloatText(
-        value=30,
+        value=0,
         description='Dipole Smooth Window (ms):',
         disabled=False,
         layout=layout,
