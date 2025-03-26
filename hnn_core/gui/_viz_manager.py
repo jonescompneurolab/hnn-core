6c49cf09748a32565fe6d51f7cc1c4db594068bf
dylansdaniels <dylansdaniels@gmail.com>
2025-03-26 11:49:58 -0400

    update comment per suggestion by gtdang!

--- hnn_core/gui/_viz_manager.py
+++ hnn_core/gui/_viz_manager.py
@@ -941,7 +941,7 @@ def _postprocess_template(template_name, fig, idx,
         return
 
     if template_name == 'Dipole Layers (3x1)':
-        # Make the L2 and L5 plots the same y-range
+        # Make the L2, L5, and aggregate plots use the same y-range
         y_lims_list = [ax.get_ylim() for ax in fig.axes]
         y_lims = (np.min(y_lims_list), np.max(y_lims_list))
         [ax.set_ylim(y_lims) for ax in fig.axes]
