aeedfef7ebc8ed3928fd732326d6be779d378dd4
Austin E. Soplata <me@asoplata.com>
2025-06-13 11:52:42 -0400

    doc: cleanup regen-base script and contrib
    This makes some small cleanliness changes:
    
    1. Removes the main usage and ruff-formats
    `dev_scripts/regenerate_base_network.py`
    2. Converts all code examples in `contributing.md` from RST to MD
    format, and removes the `$` at the beginning of each line (which was
    causing problems if you literally copy+pasted the command into zsh).
    3. Adds a middle step in the Making your first PR section about when the
    developer needs to have installed the dev version of HNN-Core.

--- dev_scripts/regenerate_base_network.py
+++ dev_scripts/regenerate_base_network.py
@@ -4,24 +4,25 @@ from pathlib import Path
 from hnn_core.params import convert_to_json
 
 
-if __name__ == "__main__":
-    top_level_dir = Path(__file__).parents[1]
+top_level_dir = Path(__file__).parents[1]
 
-    network_configs_path = Path(top_level_dir, "hnn_core", "param")
-    # This is the "flat JSON" parameter file which all of our "hierarchical JSON"
-    # network files are based off of
-    input_flat_base_network_config_file = network_configs_path.joinpath("default.json")
-    # This is the "hierarchical JSON" network file which we will build
-    output_hier_base_network_config_file = network_configs_path.joinpath("jones2009_base.json")
+network_configs_path = Path(top_level_dir, "hnn_core", "param")
+# This is the "flat JSON" parameter file which all of our "hierarchical JSON"
+# network files are based off of
+input_flat_base_network_config_file = network_configs_path.joinpath("default.json")
+# This is the "hierarchical JSON" network file which we will build
+output_hier_base_network_config_file = network_configs_path.joinpath(
+    "jones2009_base.json"
+)
 
-    print(f"""
-    Note that this will OVERWRITE the current contents of
-    '{output_hier_base_network_config_file}'
-    with a fresh generation of the base network!
-    Only use this if you know what you are doing.
-    """)
+print(f"""
+Note that this will OVERWRITE the current contents of
+'{output_hier_base_network_config_file}'
+with a fresh generation of the base network!
+Only use this if you know what you are doing.
+""")
 
-    convert_to_json(
-        input_flat_base_network_config_file,
-        output_hier_base_network_config_file,
-    )
+convert_to_json(
+    input_flat_base_network_config_file,
+    output_hier_base_network_config_file,
+)
