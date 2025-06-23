94c75178d97b2ffd84f3b52905f48730d5cc0edc
Austin E. Soplata <me@asoplata.com>
2025-06-23 13:39:38 -0400

    Incr vers, add back tests, add Pypi dist path

--- doc/conf.py
+++ doc/conf.py
@@ -33,9 +33,9 @@ author = 'HNN Developers'
 # -- Version handling --------------------------------------------------------
 
 # The short X.Y version
-version = '0.4.2.dev0'
+version = '0.4.2.dev1'
 # The full version, including alpha/beta/rc tags
-release = '0.4.2.dev0'
+release = '0.4.2.dev1'
 
 ### HTML theme version control
 # If you are making a stable release, then you should add entries to the file
