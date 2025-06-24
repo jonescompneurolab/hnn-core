a79a3dc2fa4ae7ca8ddc6d5ba793efd908f760d2
Austin E. Soplata <me@asoplata.com>
2025-06-24 14:07:30 -0400

    release: patch v0.4.2

--- doc/conf.py
+++ doc/conf.py
@@ -33,9 +33,9 @@ author = 'HNN Developers'
 # -- Version handling --------------------------------------------------------
 
 # The short X.Y version
-version = '0.4.2.dev2'
+version = '0.4.2'
 # The full version, including alpha/beta/rc tags
-release = '0.4.2.dev2'
+release = '0.4.2'
 
 ### HTML theme version control
 # If you are making a stable release, then you should add entries to the file
