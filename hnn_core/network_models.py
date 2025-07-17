179cc9da1ffaaf2c88680fda04be4752a6b838a0
katduecker <katharina.duecker@gmail.com>
2025-07-17 19:03:03 -0400

    change model name

--- hnn_core/network_models.py
+++ hnn_core/network_models.py
@@ -398,7 +398,7 @@ def calcium_model(
 
     return net
 
-def human_ET_model(params=None, add_drives_from_params=False,
+def duecker_2025_ET_model(params=None, add_drives_from_params=False,
                   legacy_mode=False, mesh_shape=(10, 10)):
 
 
