e9ba1cac9e7f03c5de2bf674dd23eb909c15ce79
Austin E. Soplata <me@asoplata.com>
2025-09-11 10:17:03 -0400

    doc: add docstring for shift

--- hnn_core/network.py
+++ hnn_core/network.py
@@ -2193,22 +2193,40 @@ class Network:
         return max_gid + 1
 
     def shift_gid_ranges(self, gid_start=0):
-        """AES TODO"""
-        # Update GID ranges to start at gid_start and avoid overlap
+        """Reset cell and non-drive GIDs to begin at a different number.
+
+        Parameters
+        ----------
+        gid_start : int, default 0
+            Number to begin the Network's non-drive GIDs at, instead of the default
+            value of 0. This number will be used to reconstruct both
+            `Network.gid_ranges` and some entries in `Network.connectivity`.
+
+        Notes
+        -----
+        - This function is intended for future work in simulating multiple `Network`
+          objects within the same simulation. Specifically, this is intended for usage
+          with the "second" or any additional `Network` objects, such that the first
+          `Network` and additional `Networks` use a disjoint set of GIDs.
+        - This successfully shifts GIDs for individual cells and for non-drive synapses,
+          but currently does NOT shift GIDs for external drives. You should shift your
+          Network's GIDs only BEFORE adding drives to the Network.
+        """
+        # Shift GID ranges to start at gid_start and avoid overlap
         current_gid = gid_start
 
         original_gid_ranges = deepcopy(self.gid_ranges)
         self.gid_ranges = OrderedDict()
 
-        # Update individual cell GIDs to match new gid_ranges
+        # Shift individual cell GIDs to match new gid_ranges
         for ct in self.cell_types.keys():
             n_cells = len(self.pos_dict[ct])
             self.gid_ranges[ct] = range(current_gid, current_gid + n_cells)
             current_gid += n_cells
 
-        # Update connectivity GIDs to match new gid_ranges
+        # Shift connectivity GIDs to match new gid_ranges
         for conn in self.connectivity:
-            # Update target_gids if target_type is in mapping
+            # Shift target_gids if target_type is in mapping
             if (
                 "target_type" in conn
                 and conn["target_type"] in self.cell_types.keys()
@@ -2218,7 +2236,7 @@ class Network:
                 n_targets = len(conn["target_gids"])
                 # Replace with correct GIDs from gid_ranges
                 conn["target_gids"] = gid_range[:n_targets]
-            # Update src_gids if src_type is in mapping
+            # Shift src_gids if src_type is in mapping
             if "src_type" in conn and conn["src_type"] in self.cell_types.keys():
                 ct = conn["src_type"]
                 gid_range = list(self.gid_ranges[ct])
