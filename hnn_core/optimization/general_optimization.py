jonescompneurolab/hnn-core

commit 6de927077aed65f5b24851a51464afe708969260

[MRG] Allow user-defined initial weights and average dipoles in obj func (#1057)
* Allow user-defined initial weights, update objective function to average 3 dipoles rather than simulate a single one, enhance optimization example

* Revert dipole averaging in _maximize_psd obj func

* Allow user to define n_trials when optimizing dipole

* Address ruff formatting test fail

* Edit example

* Address test_optimize_evoked test fail by passing in n_trials, address test_rhythmic test fail by fixing bug in _maximize_psd objective function

* Address feedback

* Add unit tests for initial_params edge cases

* Update whats_new.md Public API Changes and Changelog

hnn_core/optimization/general_optimization.py
@@ -8,6 +8,7 @@
 import numpy as np
 
 from .objective_functions import _rmse_evoked, _maximize_psd
+from ..externals.mne import _validate_type
 
 
 class Optimizer:
@@ -17,6 +18,7 @@ class Optimizer:
         tstop,
         constraints,
         set_params,
+        initial_params=None,
         solver="bayesian",
         obj_fun="dipole_rmse",
         max_iter=200,
@@ -38,6 +40,10 @@ class Optimizer:
 
             where ``net`` is a Network object and ``params`` is a dictionary
             of the parameters that will be set inside the function.
+        initial_params : dict, optional
+            Initial parameters for the objective function. Keys are parameter
+            names, values are initial parameters. The default is None.
+            If None, the parameters will be set to the midpoints of parameter ranges.
         solver : str
             The optimizer, 'bayesian' or 'cobyla'.
         obj_fun : str | func
@@ -52,6 +58,8 @@ class Optimizer:
         ----------
         constraints : dict
             The user-defined constraints.
+        initial_params : dict
+            Initial parameters for the objective function.
         max_iter : int
             The max number of calls to the objective function.
         solver : func
@@ -101,6 +109,35 @@ class Optimizer:
         else:
             self.obj_fun = obj_fun  # user-defined function
             self.obj_fun_name = None
+        # Initial weights for the objective function
+        if initial_params is not None:
+            # Check if initial_params is not a dict
+            _validate_type(initial_params, dict)
+
+            # Check if initial_params keys match constraints keys
+            if set(initial_params.keys()) != set(self.constraints.keys()):
+                raise ValueError(
+                    "The keys of initial_params must match the keys of constraints."
+                )
+
+            # Check if initial_params values contain only int or float
+            for val in initial_params.values():
+                _validate_type(val, (int, float))
+
+            # Check if initial_params values are within constraints
+            for key in initial_params:
+                if not (
+                    self.constraints[key][0]
+                    <= initial_params[key]
+                    <= self.constraints[key][1]
+                ):
+                    raise ValueError(
+                        f"Initial parameter '{key}' with value {initial_params[key]} is out of bounds {self.constraints[key]}."
+                    )
+            self.initial_params = initial_params
+        else:
+            self.initial_params = _get_initial_params(self.constraints)
+
         self.tstop = tstop
         self.net_ = None
         self.obj_ = list()
@@ -121,17 +158,22 @@ class Optimizer:
         ----------
         target : instance of Dipole (if obj_fun='dipole_rmse')
             A dipole object with experimental data.
+        n_trials : int (if obj_fun='dipole_rmse')
+            Number of trials to simulate and average.
         f_bands : list of tuples (if obj_fun='maximize_psd')
             Lower and higher limit for each frequency band.
-        relative_bandpower : tuple (if obj_fun='maximize_psd')
-            Weight for each frequency band.
+        relative_bandpower : list of float | float (if obj_fun='maximize_psd')
+            Weight for each frequency band in f_bands. If a single float is provided,
+            the same weight is applied to all frequency bands.
         scale_factor : float, optional
             The dipole scale factor.
         smooth_window_len : float, optional
             The smooth window length.
         """
-        if self.obj_fun_name == "dipole_rmse" and "target" not in obj_fun_kwargs:
-            raise Exception("target must be specified")
+        if self.obj_fun_name == "dipole_rmse" and (
+            "target" not in obj_fun_kwargs or "n_trials" not in obj_fun_kwargs
+        ):
+            raise Exception("target and n_trials must be specified")
         elif self.obj_fun_name == "maximize_psd" and (
             "f_bands" not in obj_fun_kwargs
             or "relative_bandpower" not in obj_fun_kwargs
@@ -139,15 +181,14 @@ class Optimizer:
             raise Exception("f_bands and relative_bandpower must be specified")
 
         constraints = self._assemble_constraints(self.constraints)
-        initial_params = _get_initial_params(self.constraints)
 
         opt_params, obj, net_ = self._run_opt(
             self._initial_net,
             self.tstop,
             constraints,
             self._set_params,
+            self.initial_params,
             self.obj_fun,
-            initial_params,
             self.max_iter,
             obj_fun_kwargs,
         )
@@ -191,8 +232,11 @@ class Optimizer:
         axis.set_ylabel("Objective value")
         axis.grid(visible=True)
 
-        fig.show(show)
-        return axis.get_figure()
+        if ax is None:
+            fig.show(show)
+            return axis.get_figure()
+        else:
+            return ax.get_figure()
 
 
 def _get_initial_params(constraints):
@@ -288,8 +332,8 @@ def _run_opt_bayesian(
     tstop,
     constraints,
     set_params,
-    obj_fun,
     initial_params,
+    obj_fun,
     max_iter,
     obj_fun_kwargs,
 ):
@@ -305,12 +349,15 @@ def _run_opt_bayesian(
         Parameter constraints in solver-specific format.
     set_params : func
         User-defined function that sets parameters in network drives.
-    obj_fun : func
-        The objective function.
     initial_params : dict
         Keys are parameter names, values are initial parameters.
+    obj_fun : func
+        The objective function.
     max_iter : int
         Number of calls the optimizer makes.
+    obj_fun_kwargs : dict
+        Additional arguments along with their respective values to be passed
+        to the objective function.
 
     Returns
     -------
@@ -365,8 +412,8 @@ def _run_opt_cobyla(
     tstop,
     constraints,
     set_params,
-    obj_fun,
     initial_params,
+    obj_fun,
     max_iter,
     obj_fun_kwargs,
 ):
@@ -382,12 +429,15 @@ def _run_opt_cobyla(
         Parameter constraints in solver-specific format.
     set_params : func
         User-defined function that sets parameters in network drives.
-    obj_fun : func
-        The objective function.
     initial_params : dict
         Keys are parameter names, values are initial parameters.
+    obj_fun : func
+        The objective function.
     max_iter : int
         Number of calls the optimizer makes.
+    obj_fun_kwargs : dict
+        Additional arguments along with their respective values to be passed
+        to the objective function.
 
     Returns
     -------

examples/howto/optimize_evoked.py
@@ -12,16 +12,24 @@ of the model simulation to match an experimental dipole waveform.
 #          Ryan Thorpe <ryan_thorpe@brown.edu>
 #          Mainak Jas <mjas@mgh.harvard.edu>
 
+from hnn_core.optimization import Optimizer
+from urllib.request import urlretrieve
 import os.path as op
 
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 ###############################################################################
-# Let us import hnn_core
+# 1. Import libraries and set up environment
+# ------------------------------------------
+#
+# We'll start by importing the necessary `hnn_core` modules and other libraries
+# required for this tutorial.
 
 import hnn_core
 from hnn_core import (MPIBackend, jones_2009_model, simulate_dipole,
                       read_dipole)
+from hnn_core.viz import plot_dipole
 
 hnn_core_root = op.join(op.dirname(hnn_core.__file__))
 
@@ -29,186 +37,259 @@ hnn_core_root = op.join(op.dirname(hnn_core.__file__))
 n_procs = 10
 
 ###############################################################################
-# First, we will load experimental data into Dipole object.
+# 2. Load experimental dipole
+# ---------------------------
 #
-# This is a different experiment than the one to which the base parameters were
-# tuned. So, the initial RMSE will be large, giving the optimization procedure
-# a lot to work with.
-
-from urllib.request import urlretrieve
+# Our goal is to optimize our model to match experimental data.
+#
+# We'll load a pre-recorded MEG dipole waveform, which represents early evoked
+# activity from primary somatosensory cortex in response to a brief tap to
+# the finger. This data will serve as our *target dipole* for the optimization
+# process.
 
 data_url = ('https://raw.githubusercontent.com/jonescompneurolab/hnn/master/'
-            'data/MEG_detection_data/S1_SupraT.txt')
-urlretrieve(data_url, 'S1_SupraT.txt')
-exp_dpl = read_dipole('S1_SupraT.txt')
+            'data/MEG_detection_data/yes_trial_S1_ERP_all_avg.txt')
+urlretrieve(data_url, 'yes_trial_S1_ERP_all_avg.txt')
+dipole_experimental = read_dipole('yes_trial_S1_ERP_all_avg.txt')
+
+# Plot
+fig, ax = plt.subplots(figsize=(6, 6))
+dipole_experimental.plot(ax=ax, layer='agg', show=False,
+                         color='tab:blue')
+ax.legend(['experimental'])
 
 ###############################################################################
-# Let’s then simulate the dipole with some initial parameters.
+# 3. Simulate initial dipole
+# --------------------------
+#
+# Now, we'll simulate the initial dipole using a predefined set of parameters
+# we arrived to after some hand-tuning. This simulation will provide a baseline
+# to compare against the experimental data before optimization.
 
-tstop = exp_dpl.times[-1]
-scale_factor = 3000
-smooth_window_len = 30
+net_initial = jones_2009_model()
 
-net_init = jones_2009_model()
+n_drive_cells = 1
+cell_specific = False
 
 # Proximal 1
-weights_ampa_p1 = {'L2_basket': 0.2913, 'L2_pyramidal': 0.9337,
-                   'L5_basket': 0.1951, 'L5_pyramidal': 0.3602}
-weights_nmda_p1 = {'L2_basket': 0.9240, 'L2_pyramidal': 0.0845,
-                   'L5_basket': 0.5849, 'L5_pyramidal': 0.65105}
+weights_ampa_p1 = {'L2_basket': 0.08831, 'L2_pyramidal': 0.01525,
+                   'L5_basket': 0.19934, 'L5_pyramidal': 0.00865}
 synaptic_delays_p = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
                      'L5_basket': 1., 'L5_pyramidal': 1.}
-net_init.add_evoked_drive('evprox1',
-                          mu=5.6813,
-                          sigma=20.3969,
-                          numspikes=1,
-                          location='proximal',
-                          weights_ampa=weights_ampa_p1,
-                          weights_nmda=weights_nmda_p1,
-                          synaptic_delays=synaptic_delays_p)
+net_initial.add_evoked_drive('evprox1',
+                             mu=26.61,
+                             sigma=2.47,
+                             numspikes=1,
+                             location='proximal',
+                             weights_ampa=weights_ampa_p1,
+                             synaptic_delays=synaptic_delays_p,
+                             n_drive_cells=n_drive_cells,
+                             cell_specific=cell_specific)
 
 # Distal
-weights_ampa_d1 = {'L2_basket': 0.8037, 'L2_pyramidal': 0.5738,
-                   'L5_pyramidal': 0.3626}
-weights_nmda_d1 = {'L2_basket': 0.2492, 'L2_pyramidal': 0.6183,
-                   'L5_pyramidal': 0.1121}
+weights_ampa_d1 = {'L2_basket': 0.006562, 'L2_pyramidal': .000007,
+                   'L5_pyramidal': 0.142300}
+weights_nmda_d1 = {'L2_basket': 0.019482, 'L2_pyramidal': 0.004317,
+                   'L5_pyramidal': 0.080074}
 synaptic_delays_d1 = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
                       'L5_pyramidal': 0.1}
-net_init.add_evoked_drive('evdist1',
-                          mu=58.6539,
-                          sigma=5.5810,
-                          numspikes=1,
-                          location='distal',
-                          weights_ampa=weights_ampa_d1,
-                          weights_nmda=weights_nmda_d1,
-                          synaptic_delays=synaptic_delays_d1)
+net_initial.add_evoked_drive('evdist1',
+                             mu=63.53,
+                             sigma=3.85,
+                             numspikes=1,
+                             location='distal',
+                             weights_ampa=weights_ampa_d1,
+                             weights_nmda=weights_nmda_d1,
+                             synaptic_delays=synaptic_delays_d1,
+                             n_drive_cells=n_drive_cells,
+                             cell_specific=cell_specific)
 
 # Proximal 2
-weights_ampa_p2 = {'L2_basket': 0.01, 'L2_pyramidal': 0.01, 'L5_basket': 0.01,
-                   'L5_pyramidal': 0.01}
-weights_nmda_p2 = {'L2_basket': 0.01, 'L2_pyramidal': 0.01, 'L5_basket': 0.01,
-                   'L5_pyramidal': 0.01}
-net_init.add_evoked_drive('evprox2',
-                          mu=80,
-                          sigma=1,
-                          numspikes=1,
-                          location='proximal',
-                          weights_ampa=weights_ampa_p2,
-                          weights_nmda=weights_nmda_p2,
-                          synaptic_delays=synaptic_delays_p)
+weights_ampa_p2 = {'L2_basket': 0.000003, 'L2_pyramidal': 1.438840,
+                   'L5_basket': 0.008958, 'L5_pyramidal': 0.1}
+net_initial.add_evoked_drive('evprox2',
+                             mu=120.,
+                             sigma=1.,
+                             numspikes=1,
+                             location='proximal',
+                             weights_ampa=weights_ampa_p2,
+                             synaptic_delays=synaptic_delays_p,
+                             n_drive_cells=n_drive_cells,
+                             cell_specific=cell_specific)
 
+# Simulate initial dipole
+n_trials = 3
+tstop = dipole_experimental.times[-1]
 with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
-    init_dpl = simulate_dipole(net_init, tstop=tstop, n_trials=1)[0]
-init_dpl.scale(scale_factor)
-init_dpl.smooth(smooth_window_len)
+    dipoles_initial = simulate_dipole(
+        net_initial, tstop=tstop, n_trials=n_trials)
+
+# Smooth and scale
+window_length = 30
+scaling_factor = 3000
+for dipole in dipoles_initial:
+    dipole.smooth(window_length).scale(scaling_factor)
+
+# Plot
+fig, ax = plt.subplots(figsize=(6, 6))
+dipole_experimental.plot(ax=ax, layer='agg', show=False,
+                         color='tab:blue')
+plot_dipole(dipoles_initial.copy(), ax=ax, layer='agg', show=False,
+            color='tab:orange', average=True)
+
+# Legend
+legend_handles = [Line2D([0], [0], color='tab:blue', lw=1.0),
+                  Line2D([0], [0], color='tab:orange', lw=1.0)]
+ax.legend(legend_handles, ['experimental', 'initial'])
 
 ###############################################################################
-# Now we start the optimization!
+# 4. Define the ``set_params`` function
+# -----------------------------------
 #
-# First, we define a function that will tell the optimization routine how to
-# modify the network drive parameters. The function will take in the Network
-# object with no attached drives, and a dictionary of the parameters we wish to
-# optimize.
+# Now we can begin the optimization!
+#
+# First, we'll define a ``set_params`` function. This function tells the optimizer
+# how to apply the chosen parameters to the network during each iteration.
+#
+# Comparison of the initial dipoles reveals that the second positive peak of the
+# experimental dipole (blue) has a larger amplitude, later latency, and broader
+# temporal distribution than the corresponding peak in the initial dipole (orange).
+#
+# Let's optimize the relevant parameters for the second proximal (evprox2) drive,
+# as it is primarily responsible for this component of the dipole.
 
 
 def set_params(net, params):
 
     # Proximal 1
     net.add_evoked_drive('evprox1',
-                         mu=5.6813,
-                         sigma=20.3969,
+                         mu=26.61,
+                         sigma=2.47,
                          numspikes=1,
                          location='proximal',
                          weights_ampa=weights_ampa_p1,
-                         weights_nmda=weights_nmda_p1,
-                         synaptic_delays=synaptic_delays_p)
+                         synaptic_delays=synaptic_delays_p,
+                         n_drive_cells=n_drive_cells,
+                         cell_specific=cell_specific)
 
     # Distal
     net.add_evoked_drive('evdist1',
-                         mu=58.6539,
-                         sigma=5.5810,
+                         mu=63.53,
+                         sigma=3.85,
                          numspikes=1,
                          location='distal',
                          weights_ampa=weights_ampa_d1,
                          weights_nmda=weights_nmda_d1,
-                         synaptic_delays=synaptic_delays_d1)
+                         synaptic_delays=synaptic_delays_d1,
+                         n_drive_cells=n_drive_cells,
+                         cell_specific=cell_specific)
 
     # Proximal 2
-    weights_ampa_p2 = {'L2_basket':
-                       params['evprox2_ampa_L2_basket'],
-                       'L2_pyramidal':
-                       params['evprox2_ampa_L2_pyramidal'],
-                       'L5_basket':
-                       params['evprox2_ampa_L5_basket'],
-                       'L5_pyramidal':
-                       params['evprox2_ampa_L5_pyramidal']}
-    weights_nmda_p2 = {'L2_basket':
-                       params['evprox2_nmda_L2_basket'],
-                       'L2_pyramidal':
-                       params['evprox2_nmda_L2_pyramidal'],
-                       'L5_basket':
-                       params['evprox2_nmda_L5_basket'],
-                       'L5_pyramidal':
-                       params['evprox2_nmda_L5_pyramidal']}
+    weights_ampa_p2 = {'L2_basket': 0.000003, 'L2_pyramidal': 1.438840,
+                       'L5_basket': 0.008958, 'L5_pyramidal': params['evprox2_ampa_L5_pyramidal']}
     net.add_evoked_drive('evprox2',
                          mu=params['evprox2_mu'],
                          sigma=params['evprox2_sigma'],
                          numspikes=1,
                          location='proximal',
                          weights_ampa=weights_ampa_p2,
-                         weights_nmda=weights_nmda_p2,
-                         synaptic_delays=synaptic_delays_p)
+                         synaptic_delays=synaptic_delays_p,
+                         n_drive_cells=n_drive_cells,
+                         cell_specific=cell_specific)
 
 ###############################################################################
-# Then, we define the constraints.
+# 5. Define optimization constraints
+# ----------------------------------
 #
-# The constraints must be a dictionary of tuples where the first value in each
-# tuple is the lower bound and the second value is the upper bound for the
-# corresponding parameter.
+# The constraints define the permissible range (lower and upper bounds) for each
+# parameter during the optimization process.
 #
-# The following synaptic weight parameter ranges (units of micro-siemens)
-# were chosen so as to keep the model in physiologically realistic regimes.
+# The ranges for synaptic weights (µS) are set to maintain physiologically
+# realistic model behavior, rather than solely relying on existing literature values.
 
 
-constraints = dict({'evprox2_ampa_L2_basket': (0.01, 1.),
-                    'evprox2_ampa_L2_pyramidal': (0.01, 1.),
-                    'evprox2_ampa_L5_basket': (0.01, 1.),
-                    'evprox2_ampa_L5_pyramidal': (0.01, 1.),
-                    'evprox2_nmda_L2_basket': (0.01, 1.),
-                    'evprox2_nmda_L2_pyramidal': (0.01, 1.),
-                    'evprox2_nmda_L5_basket': (0.01, 1.),
-                    'evprox2_nmda_L5_pyramidal': (0.01, 1.),
-                    'evprox2_mu': (100., 120.),
-                    'evprox2_sigma': (2., 30.)})
+constraints = dict({'evprox2_ampa_L5_pyramidal': (0.01, 1.0),
+                    'evprox2_mu': (100., 150.),
+                    'evprox2_sigma': (1., 20.)})
 
 ###############################################################################
-# Now we define and fit the optimizer.
+# 6. Define the initial parameters
+# --------------------------------
+#
+# For optimal results, set initial_params to your hand-tuned values since they
+# already provide a good fit.
+#
+# If ``initial_params`` is not set, the optimizer will use the midpoint of the
+# constraints as the initial parameters.
 
-from hnn_core.optimization import Optimizer
+initial_params = dict({'evprox2_ampa_L5_pyramidal': 0.1,
+                       'evprox2_mu': 120.,
+                       'evprox2_sigma': 1.})
+
+###############################################################################
+# 7. Initialize and run the optimizer
+# -----------------------------------
+#
+# Finally, let's initialize and run the optimizer. We'll instantiate the Optimizer
+# class, providing our network, simulation time, constraints, and the ``set_params``
+# function.
+#
+# By default, the Optimizer aims to minimize the Root Mean Square Error (RMSE)
+# between the simulated and experimental dipoles.
+#
+# To capture the model's average behavior, it's recommended to set ``n_trials``
+# to a value greater than 1. Using ``n_trials=1`` might find a parameter set that
+# works well for a single, specific simulation run but performs poorly on average.
+#
+# Additionally, while ``max_iter`` is set to 50 in this example for a quicker
+# demonstration, you can set it to any value. The default is 200.
+#
+# .. note:: A custom objective function can also be supplied.
 
 net = jones_2009_model()
 optim = Optimizer(net, tstop=tstop, constraints=constraints,
-                  set_params=set_params)
+                  set_params=set_params, initial_params=initial_params, max_iter=50)
 with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
-    optim.fit(target=exp_dpl, scale_factor=scale_factor,
-              smooth_window_len=smooth_window_len)
+    optim.fit(target=dipole_experimental, n_trials=n_trials, scale_factor=scaling_factor,
+              smooth_window_len=window_length)
 
 ###############################################################################
-# Finally, we can plot the experimental data alongside the post-optimization
-# simulation dipole as well as the convergence plot.
+# 8. Simulate the optimized dipole
+# --------------------------------
+#
+# Now, we can simulate the dipole using the newly found optimized parameters
+# to see how well they match the experimental data.
 
 with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
-    opt_dpl = simulate_dipole(optim.net_, tstop=tstop, n_trials=1)[0]
-opt_dpl.scale(scale_factor)
-opt_dpl.smooth(smooth_window_len)
+    dipoles_optimized = simulate_dipole(
+        optim.net_, tstop=tstop, n_trials=n_trials)
 
-fig, axes = plt.subplots(2, 1, sharex=True, figsize=(6, 6))
+# Smooth and scale
+for dipole in dipoles_optimized:
+    dipole.smooth(window_length).scale(scaling_factor)
 
-# plot original
-exp_dpl.plot(ax=axes[0], layer='agg', show=False, color='tab:blue')
-init_dpl.plot(ax=axes[0], layer='agg', show=False, color='tab:orange')
-opt_dpl.plot(ax=axes[0], layer='agg', show=False, color='tab:green')
-axes[0].legend(['experimental', 'initial', 'optimized'])
-optim.net_.cell_response.plot_spikes_hist(ax=axes[1])
+###############################################################################
+# 9. Visualize the results
+# ------------------------
+#
+# Finally, we can compare the experimental, initial, and optimized dipoles.
+#
+# We can also plot the convergence.
 
-fig1 = optim.plot_convergence()
+fig, ax = plt.subplots(sharex=True, figsize=(6, 6))
+dipole_experimental.plot(ax=ax, layer='agg', show=False,
+                         color='tab:blue')
+plot_dipole(dipoles_initial.copy(), ax=ax, layer='agg', show=False,
+            color='tab:orange', average=True)
+plot_dipole(dipoles_optimized.copy(), ax=ax, layer='agg',
+            show=False, color='tab:green', average=True)
+
+# Legend
+legend_handles = [Line2D([0], [0], color='tab:blue', lw=1.0),
+                  Line2D([0], [0], color='tab:orange', lw=1.0),
+                  Line2D([0], [0], color='tab:green', lw=1.0)]
+ax.legend(legend_handles, ['experimental', 'initial', 'optimized'])
+
+# Convergence plot
+optim.plot_convergence()
+plt.show()

examples/howto/optimize_rhythmic.py
@@ -10,6 +10,8 @@ components.
 
 # Authors: Carolina Fernandez <cxf418@miami.edu>
 
+from hnn_core.viz import plot_psd
+from hnn_core.optimization import Optimizer
 import matplotlib.pyplot as plt
 
 ###############################################################################
@@ -25,6 +27,7 @@ n_procs = 10
 # modify the network drive parameters. The function will take in the Network
 # object with no attached drives, and a dictionary of the parameters we wish to
 # optimize.
+
 
 def set_params(net, params):
 
@@ -70,6 +73,7 @@ def set_params(net, params):
 # The following synaptic weight parameter ranges (units of micro-siemens)
 # were chosen so as to keep the model in physiologically realistic regimes.
 
+
 constraints = dict()
 constraints.update({'alpha_prox_weight': (4.4e-5, 6.4e-5),
                     'alpha_prox_tstart': (45, 55),
@@ -82,7 +86,6 @@ constraints.update({'alpha_prox_weight': (4.4e-5, 6.4e-5),
 
 ###############################################################################
 # Now we define and fit the optimizer.
-from hnn_core.optimization import Optimizer
 
 tstop = 300
 scale_factor = 3000
@@ -90,19 +93,18 @@ smooth_window_len = 20
 
 net = jones_2009_model()
 optim = Optimizer(net, tstop=tstop, constraints=constraints,
-                  set_params=set_params, obj_fun='maximize_psd')
+                  set_params=set_params, obj_fun='maximize_psd', max_iter=50)
 
 # 8-15 Hz (alpha) and 15-30 Hz (beta) are the frequency bands whose
 # power we wish to maximize in a ratio of 1 to 2.
 with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
-    optim.fit(f_bands=[(9, 11), (19, 21)], relative_bandpower=(1, 2),
+    optim.fit(f_bands=[(9, 11), (19, 21)], relative_bandpower=[1, 2],
               scale_factor=scale_factor, smooth_window_len=smooth_window_len)
 
 ###############################################################################
 # Finally, we can plot the optimized dipole, power spectral density (PSD), and
 # convergence plot.
 
-from hnn_core.viz import plot_psd
 
 with MPIBackend(n_procs=n_procs, mpi_cmd='mpiexec'):
     opt_dpl = simulate_dipole(optim.net_, tstop=tstop, n_trials=1)[0]
@@ -119,4 +121,5 @@ axes[0].legend(['optimized'])
 plot_psd(opt_dpl, fmax=50, ax=axes[1], show=False)
 
 # convergence
-fig1 = optim.plot_convergence()
+optim.plot_convergence()
+plt.show()

commit ca2eb3a43edba5a5c3bad3724362b1d698b4229d

fix: opt: use safe default n_trials for rmse opt
This addresses the optimization parameter regression-bug accidentally
introduced by #1057 , where n_trials was changed to being explicitly
required to be set by the user if using `dipole_rmse`
optimization. Instead, n_trials is set by 1 if not provided.

hnn_core/optimization/general_optimization.py
@@ -156,13 +156,13 @@ class Optimizer:
 
         Parameters
         ----------
-        target : instance of Dipole (if obj_fun='dipole_rmse')
+        target : instance of Dipole (Required if obj_fun='dipole_rmse')
             A dipole object with experimental data.
-        n_trials : int (if obj_fun='dipole_rmse')
+        n_trials : int (Optional if obj_fun='dipole_rmse')
             Number of trials to simulate and average.
-        f_bands : list of tuples (if obj_fun='maximize_psd')
+        f_bands : list of tuples (Required if obj_fun='maximize_psd')
             Lower and higher limit for each frequency band.
-        relative_bandpower : list of float | float (if obj_fun='maximize_psd')
+        relative_bandpower : list of float | float (Required if obj_fun='maximize_psd')
             Weight for each frequency band in f_bands. If a single float is provided,
             the same weight is applied to all frequency bands.
         scale_factor : float, optional
@@ -170,10 +170,12 @@ class Optimizer:
         smooth_window_len : float, optional
             The smooth window length.
         """
-        if self.obj_fun_name == "dipole_rmse" and (
-            "target" not in obj_fun_kwargs or "n_trials" not in obj_fun_kwargs
-        ):
-            raise Exception("target and n_trials must be specified")
+        if self.obj_fun_name == "dipole_rmse":
+            if "target" not in obj_fun_kwargs:
+                raise Exception("target must be specified")
+            elif "n_trials" not in obj_fun_kwargs:
+                obj_fun_kwargs["n_trials"] = 1
+
         elif self.obj_fun_name == "maximize_psd" and (
             "f_bands" not in obj_fun_kwargs
             or "relative_bandpower" not in obj_fun_kwargs