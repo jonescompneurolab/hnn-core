a3397d9790cf0922f5b3d71e10618d4010d55b16
katduecker <katharina.duecker@gmail.com>
2025-11-03 17:18:53 -0500

    update bugs read_params

--- hnn_core/drives.py
+++ hnn_core/drives.py
@@ -236,10 +236,12 @@ def _add_drives_from_params(net):
         _tstop = bias_specs["tonic"][cellname]["tstop"]
         net.add_tonic_bias(amplitude=_cell_types_amplitudes, t0=_t0, tstop=_tstop)
 
-    # in HNN-GUI, seed is determined by "absolute GID" instead of the
-    # gid offset with respect to the first cell of a population.
-    for drive_name, drive in net.external_drives.items():
-        drive["event_seed"] += net.gid_ranges[drive_name][0]
+    # KD 11/03: this is a problem for reproducibility in HNN core, as this is only done when loading params from .json files,
+    # so the seeds between the original simulation and loaded simulation will differ. Do we need this?
+    # # in HNN-GUI, seed is determined by "absolute GID" instead of the
+    # # gid offset with respect to the first cell of a population.
+    # for drive_name, drive in net.external_drives.items():
+    #     drive["event_seed"] += net.gid_ranges[drive_name][0]
 
 
 def _get_prng(seed, gid):
