4ed7c936c000ec9528f958ea713b12a4aed10c67
Austin E. Soplata <me@asoplata.com>
2025-12-12 15:31:18 -0500

    ref: rename survey, change to module install path

--- hnn_core/gui/__init__.py
+++ hnn_core/gui/__init__.py
@@ -1,10 +1,1 @@
 from .gui import HNNGUI, launch
-
-def _maybe_check_first_run():
-    try:
-        from ..first_run import check_first_run
-        check_first_run()
-    except ImportError as e:
-        print("Warning: could not run first-run check:", e)
-
-_maybe_check_first_run()
