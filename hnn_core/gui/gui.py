064b90e0b5513abaed8e0ec83ae52a4d6cb56a99
dylansdaniels <dylansdaniels@gmail.com>
2026-02-26 18:05:39 -0500

    codespell fix

--- hnn_core/gui/gui.py
+++ hnn_core/gui/gui.py
@@ -1981,7 +1981,7 @@ yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
                     pointer-events: none;
                     background-color: var(--jp-widgets-input-color) !important;
 
-                    /* spliting long strings for ruff */
+                    /* splitting long strings for ruff */
                     -webkit-mask-image: url('data:image/svg+xml;utf8,<svg """
                 """xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">"""
                 """<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>"""
