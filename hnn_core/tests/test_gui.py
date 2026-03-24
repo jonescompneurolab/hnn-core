6d099a4619163daab6e8652766a588ef02b8f416
Yaswanth8390 <0515yaswanth@gmail.com>
2026-03-24 23:38:24 +0530

    Add GUI test for spectrogram trial averaging behavior

--- hnn_core/tests/test_gui.py
+++ hnn_core/tests/test_gui.py
@@ -566,6 +566,53 @@ def test_gui_add_figure(setup_gui):
     assert remaining_titles1 == remaining_titles2 == correct_remaining_titles
     plt.close("all")
 
+def test_avg_spectrogram_data(setup_gui):
+    """Test whether the spectrogram plot data is consistent across simulations on various trials"""
+    gui = setup_gui
+    gui.widget_tstop.value = 500
+    simulations = [("sim1", 1), ("sim2", 1), ("sim3", 2)]
+
+    # run simulations for single and multi trials
+    for sim_name, trials in simulations:
+        gui.widget_simulation_name.value = sim_name
+        gui.widget_ntrials.value = trials
+        gui.run_button.click()
+
+    gui._simulate_viz_action("switch_fig_template", "[Blank] single figure")
+    gui._simulate_viz_action("add_fig")
+
+    figid = gui.viz_manager.fig_idx["idx"] - 1
+    figname = f"Figure {figid}"
+    axname = "ax0"
+
+    spectrograms = {}
+
+    for sim_name, _ in simulations:
+
+        # clear axis first and then plot the spectrogram
+        gui._simulate_viz_action(
+            "edit_figure", figname, axname, sim_name, "spectrogram", {}, "clear"
+        )
+        gui._simulate_viz_action(
+            "edit_figure", figname, axname, sim_name, "spectrogram", {}, "plot"
+        )
+
+        fig = gui.viz_manager.figs[figid]
+        found = False
+        for ax in fig.axes:
+            if len(ax.collections) > 0:
+                spectrograms[sim_name] = ax.collections[0].get_array().copy()
+                found = True
+                break
+        assert found, "No spectrogram data found"
+
+    # equality for single-trial simulations
+    assert np.allclose(spectrograms["sim1"], spectrograms["sim2"])
+    # equality for multi-trial simulations
+    assert not np.allclose(spectrograms["sim1"], spectrograms["sim3"])
+
+    plt.close("all")
+
 
 def test_gui_add_data_dependent_figure(setup_gui):
     """Test if the GUI adds/deletes figs data dependent properly."""
