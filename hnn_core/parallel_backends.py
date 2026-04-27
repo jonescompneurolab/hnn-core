1cdd089911eb350e8339e22be447ef7033f65e32
Austin E. Soplata <me@asoplata.com>
2026-04-27 14:06:55 -0400

    DBG: add script and param change to repro MPI timeout

--- hnn_core/parallel_backends.py
+++ hnn_core/parallel_backends.py
@@ -996,7 +996,7 @@ class MPIBackend(object):
         self.proc, sim_data = run_subprocess(
             command=self.mpi_cmd,
             obj=[net, tstop, dt, n_trials],
-            timeout=60,
+            timeout=1,
             proc_queue=self.proc_queue,
             env=env,
             cwd=os.getcwd(),
