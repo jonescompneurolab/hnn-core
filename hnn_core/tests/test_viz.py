12a69c204a2af3ffbe474664ba86f28ed4263cc1
Austin E. Soplata <me@asoplata.com>
2026-09-11 10:06:52 -0400

    REF hist invert_spike plot tests with other hist tests

--- hnn_core/tests/test_viz.py
+++ hnn_core/tests/test_viz.py
@@ -282,16 +282,19 @@ class TestDipoleViz:
         assert len(fig.axes[0].collections) > 0, "No data plotted in raster plot"
 
     def test_dipole_viz_cell_response_plot_spikes_hist(self, run_simulation):
-        """Test spike histogram plotting with trial_idx and color argument variations."""
+        """Test spike histogram plotting across its arguments."""
         net, _ = run_simulation
         net.cell_response.plot_spikes_hist()
         # simulation second run. first one it's in the run_simulation definition
         simulate_dipole(net, tstop=100.0, n_trials=2, record_vsec="all")
 
+        # Test trial_idx arg
         with pytest.raises(TypeError, match="trial_idx must be an instance of"):
             net.cell_response.plot_spikes_hist(trial_idx="blah")
         net.cell_response.plot_spikes_hist(trial_idx=0, show=False)
         net.cell_response.plot_spikes_hist(trial_idx=[0, 1], show=False)
+
+        # Test color arg
         net.cell_response.plot_spikes_hist(color="r")
         net.cell_response.plot_spikes_hist(color=["C0", "C1"])
         net.cell_response.plot_spikes_hist(color={"beta_prox": "r", "beta_dist": "g"})
@@ -316,6 +319,49 @@ class TestDipoleViz:
             )
         with pytest.raises(ValueError, match="'beta_dist' must be"):
             net.cell_response.plot_spikes_hist(color={"beta_prox": "r"})
+
+        # Test invert_spike_types arg
+        def _check_inverted_spike_axes(fig):
+            # check that there are 2 y axes
+            assert len(fig.axes) == 2
+
+            # check for equivalency of both y axes
+            y1 = fig.axes[0]
+            y2 = fig.axes[1]
+
+            y1_max = max(y1.get_ylim())
+            y2_max = max(y2.get_ylim())
+
+            assert y1_max == y2_max
+
+            # check that data are plotted
+            assert y1_max > 1
+
+        # Test invert_spike_types
+        # str input
+        fig = net.cell_response.plot_spikes_hist(
+            spike_types=["beta_prox", "beta_dist"],
+            invert_spike_types="beta_prox",
+            show=False,
+        )
+        _check_inverted_spike_axes(fig)
+
+        # single-element list input
+        fig = net.cell_response.plot_spikes_hist(
+            spike_types=["beta_prox", "beta_dist"],
+            invert_spike_types=["beta_prox"],
+            show=False,
+        )
+        _check_inverted_spike_axes(fig)
+
+        # test case where all inputs are flipped
+        fig = net.cell_response.plot_spikes_hist(
+            spike_types=["beta_prox", "beta_dist"],
+            invert_spike_types=["beta_prox", "beta_dist"],
+            show=False,
+        )
+        _check_inverted_spike_axes(fig)
+
         plt.close("all")
 
 
@@ -878,75 +924,5 @@ def test_network_plotter_export(tmp_path, setup_net):
     net_plot.export_movie(path_out, dpi=200, decim=100, writer="pillow")
 
     assert path_out.is_file()
-
-    plt.close("all")
-
-
-def test_invert_spike_types(setup_net):
-    """Test plotting a histogram with an inverted external drive"""
-    net = setup_net
-
-    weights_ampa = {"L2_pyramidal": 0.15, "L5_pyramidal": 0.15}
-    syn_delays = {"L2_pyramidal": 0.1, "L5_pyramidal": 1.0}
-
-    net.add_evoked_drive(
-        "evdist1",
-        mu=63.53,
-        sigma=3.85,
-        numspikes=1,
-        weights_ampa=weights_ampa,
-        location="distal",
-        synaptic_delays=syn_delays,
-        event_seed=274,
-    )
-
-    net.add_evoked_drive(
-        "evprox1",
-        mu=26.61,
-        sigma=2.47,
-        numspikes=1,
-        weights_ampa=weights_ampa,
-        location="proximal",
-        synaptic_delays=syn_delays,
-        event_seed=274,
-    )
-
-    _ = simulate_dipole(net, dt=0.5, tstop=80.0, n_trials=1)
-
-    # test string input
-    net.cell_response.plot_spikes_hist(
-        spike_types=["evprox", "evdist"],
-        invert_spike_types="evdist",
-        show=False,
-    )
-
-    # test case where all inputs are flipped
-    net.cell_response.plot_spikes_hist(
-        spike_types=["evprox", "evdist"],
-        invert_spike_types=["evprox", "evdist"],
-        show=False,
-    )
-
-    # test case where some inputs are flipped
-    fig = net.cell_response.plot_spikes_hist(
-        spike_types=["evprox", "evdist"],
-        invert_spike_types=["evdist"],
-        show=False,
-    )
-
-    # check that there are 2 y axes
-    assert len(fig.axes) == 2
-
-    # check for equivalency of both y axes
-    y1 = fig.axes[0]
-    y2 = fig.axes[1]
-
-    y1_max = max(y1.get_ylim())
-    y2_max = max(y2.get_ylim())
-
-    assert y1_max == y2_max
-
-    # check that data are plotted
-    assert y1_max > 1
 
     plt.close("all")
